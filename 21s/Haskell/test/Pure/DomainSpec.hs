788993331474f144557fa5c6bf41e31376fa013b
Enrico Benini <ebenini@mdsol.com>
2019-02-25 14:44:51 +0000

    Domain test spec done

--- 21s/Haskell/test/Pure/DomainSpec.hs
+++ 21s/Haskell/test/Pure/DomainSpec.hs
@@ -1,9 +1,45 @@
 module Pure.DomainSpec where
 
+import Pure.Domain
 import Test.Hspec
 
-spec :: Spec
-spec = describe "Pure.Domain" $ do it "works" $ do True `shouldBe` True
+cardsValues :: [(CardValue, Int)]
+cardsValues = [
+  (Two, 2),
+  (Three  , 3),
+  (Four   , 4),
+  (Five   , 5),
+  (Six    , 6),
+  (Seven  , 7),
+  (Eight  , 8),
+  (Nine   , 9),
+  (Ten    , 10),
+  (Jack   , 10),
+  (King   , 10),
+  (Queen  , 10),
+  (Ace    , 11)
+  ]
 
+spec :: Spec
+spec =
+  describe "Domain" $ do
+    describe "to21Value" $ do
+      it "return the expected value" $
+       mapM_ (\t -> to21Value (fst t) `shouldBe` (snd t) ) cardsValues
+    describe "sam" $ do
+      it "should have an empty hand" $
+        (hand sam) `shouldBe` []
+      it "should have a name as \"Sam\"" $
+        (name sam) `shouldBe` "Sam"
+    describe "dealer" $ do
+      it "should have an empty hand" $
+        (hand dealer) `shouldBe` []
+      it "should have a name as \"Dealer\"" $
+        (name dealer) `shouldBe` "Dealer"
+    describe "deck" $ do
+      it "should have 52 cards" $
+        (length deck) `shouldBe` 52
+      it "shold have 12 cards foreach card suit" $
+        mapM_ (\ct -> length (filter ((== ct) . cType) deck) `shouldBe` 13) ((enumFrom (toEnum 0)) :: [CardType])
 main :: IO ()
 main = hspec spec
