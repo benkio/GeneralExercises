11ccc5acf2fbb9154aeba8aa2877df8527993d36
Enrico Benini <ebenini@mdsol.com>
2019-03-15 18:02:53 +0000

    Remove useless instances

--- 21s/Haskell/test/IO/EffectfulInstances.hs
+++ 21s/Haskell/test/IO/EffectfulInstances.hs
@@ -6,38 +6,21 @@ module IO.EffectfulInstances where
 import qualified Control.Monad.State.Lazy as S
 import Control.Monad.State.Class
 import Control.Monad.State.Lazy
+import Control.Exception
 import Pure.Domain
 import IO.Algebras
 import Control.Monad.Random
+import Control.Monad.Except
 import Control.Monad.Trans.Random.Lazy
 import Data.Functor.Identity
 
--- newtype TestStack m a = TestStack (S.StateT GameState m a)
---   deriving (Functor, Applicative, Monad)
-
--- testStack :: TestStack TestConsole ()
--- testStack = state (\gs -> ((), gs)) 
-
-newtype TestConsole a = TestConsole (S.State [String] a)
+newtype TestConsole m a = TestConsole (S.StateT [String] m a)
   deriving (Functor, Applicative, Monad)
 
-testConsole :: a -> TestConsole a
+testConsole :: Monad m => a -> TestConsole m a
 testConsole a = return a
 
--- instance S.MonadTrans TestStack where
---   lift = S.lift
-
--- instance MonadState GameState (TestStack TestConsole) where
---     get = S.get
---     put = S.put
---     state = S.state
-
-instance MonadState [String] TestConsole where
-  get = S.get
-  put = S.put
-  state = S.state
-
-instance MonadConsole (S.StateT [String] Identity)  where
+instance Monad m => MonadConsole (S.StateT [String] m)  where
   putStrLn s = modify $ \st -> s:st
 
 instance RandomGen () where
