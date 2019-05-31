0cf8d273fe8e37a57a270bdb61fb9a6fa22c2aac
Enrico Benini <ebenini@mdsol.com>
2019-05-31 17:18:03 +0100

    Add the Duration parsing, replace time with hourglass

--- phoneCompany/haskell/src/Pure/Domain.hs
+++ phoneCompany/haskell/src/Pure/Domain.hs
@@ -1,10 +1,15 @@
 module Pure.Domain(
   CallLog(..),
   Call(..),
-  number) where
+  number,
+  parseDuration) where
 
-import           Data.Time.Clock
-import           Data.Text
+import           Data.Hourglass
+import           Data.List.Split
+import           Data.Int
+import           Data.Text (Text, pack)
+import           Text.Regex
+import           Text.Read
 
 data CallLog = CallLog { clCostumerId :: String,
                          clCalled :: String,
@@ -12,9 +17,21 @@ data CallLog = CallLog { clCostumerId :: String,
 
 data Call = Call { cCostumerId :: String,
                    cCalled :: Number,
-                   cDuration :: NominalDiffTime }
+                   cDuration :: Duration }
 
 type Number = Text
 
-number :: String -> Maybe Number
-number = undefined
+number :: String -> Maybe [Text]
+number s = (fmap . fmap) pack (matchRegex (mkRegex "([0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9])") s)
+
+-- String in format hh:mm:ss
+parseDuration :: String -> Maybe Duration
+parseDuration s = do
+  let sp = splitOn ":" s
+  stringParsed <- if (Prelude.length sp == 3) then Just sp else Nothing
+  resultList <-  traverse (\s -> readMaybe s :: Maybe Int64) stringParsed
+  return Duration {
+     durationHours = Hours $ resultList !! 0       -- ^ number of hours
+    , durationMinutes = Minutes $ resultList !! 1     -- ^ number of minutes
+    , durationSeconds = Seconds $ resultList !! 2     -- ^ number of seconds
+    , durationNs = NanoSeconds 0}
