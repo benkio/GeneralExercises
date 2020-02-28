b8f86df440421a0124647e4252b1b930025ab04c
benkio <benkio89@gmail.com>
2020-02-28 10:15:45 +0000

    Start Integration tests, todo Add servant-client and UI (purescript)

---
+++ Watchlist/Haskell/test/IntegrationTestSpec.hs
@@ -0,0 +1,11 @@
+module IntegrationTestSpec where
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Integration Test" $ do
+    it "test something" $ pending
