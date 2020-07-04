073e38a32a1c2599b7918edfcb5b9548b1ada216
Enrico Benini <ebenini@mdsol.com>
2020-07-04 12:54:08 +0100

    Started splitting the monster. tests still pass

--- GildedRoseKata/scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ GildedRoseKata/scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,57 +2,72 @@ package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
 
+  def initialItemQualityIncreaseOrDecrease(item: Item): Item =
+    if (!item.name.equals("Aged Brie")
+      && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      itemQualityDecrease(item)
+    } else {
+
+      itemQualityIncrease(item)
+
+      concertSpecialQualityIncrease(item)
+
+      item
+    }
+
+  def concertSpecialQualityIncrease(item: Item): Item =
+    if (item.quality < 50 && item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (item.sellIn < 11) {
+        itemQualityIncrease(item)
+      }
+
+      if (item.sellIn < 6) {
+        itemQualityIncrease(item)
+      }
+      item
+    } else item
+
+  def sellInDecrease(item: Item): Item =
+    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+      item.sellIn = item.sellIn - 1
+      item
+    } else item
+
+  def itemQualityIncrease(item: Item): Item =
+    if (item.quality < 50) {
+      item.quality = item.quality + 1
+      item
+    } else item
+
+  def itemQualityDecrease(item: Item) =
+    if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+      item.quality = item.quality - 1
+      item
+    } else item
+
+  def negativeSellInItemQualityAdjustment(item: Item): Item =
+    if (item.sellIn < 0) {
+      if (!item.name.equals("Aged Brie")) {
+        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          itemQualityDecrease(item)
+        } else {
+          item.quality = 0
+        }
+      } else {
+        itemQualityIncrease(item)
+      }
+      item
+    } else item
 
   def updateQuality() {
     for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
-          }
-        }
-      } else {
-        if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
+      val item1: Item = initialItemQualityIncreaseOrDecrease(items(i))
 
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
+      val item2: Item = sellInDecrease(item1)
 
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-          }
-        }
-      }
+      val item3: Item = negativeSellInItemQualityAdjustment(item2)
 
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
-
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
-              }
-            }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
-          }
-        } else {
-          if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
-          }
-        }
-      }
+      items(i) = item3
     }
   }
 }
