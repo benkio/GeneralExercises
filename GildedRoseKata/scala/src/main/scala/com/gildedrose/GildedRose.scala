9614fcbbf701fa45334f39eab8ef9899c16b7483
Enrico Benini <ebenini@mdsol.com>
2020-07-04 14:36:30 +0100

    Introduced new types for quality and sellin

--- GildedRoseKata/scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ GildedRoseKata/scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,76 +2,88 @@ package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
 
-  def initialItemQualityIncreaseOrDecrease(item: Item): Item =
-    if (!item.name.equals("Aged Brie")
-      && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      itemQualityDecrease(item)
+  def initialItemQualityIncreaseOrDecrease(
+    itemQuality: ItemQuality,
+    itemName: String,
+    itemSellIn: Int
+  ): ItemQuality =
+    if (!itemName.equals("Aged Brie")
+      && !itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      itemQualityDecrease(itemQuality, itemName)
     } else {
 
-      val itemQualityIncreased = itemQualityIncrease(item)
+      val itemQualityIncreased: ItemQuality = itemQualityIncrease(itemQuality)
 
-      val itemQualityIncreasedSpecial = concertSpecialQualityIncrease(itemQualityIncreased)
+      val itemQualityIncreasedSpecial: ItemQuality = concertSpecialQualityIncrease(itemQualityIncreased, itemName, itemSellIn)
 
       itemQualityIncreasedSpecial
     }
 
-  def concertSpecialQualityIncrease(item: Item): Item =
-    if (item.quality < 50 && item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      item.sellIn match {
-        case x if x < 6 => itemQualityIncrease(itemQualityIncrease(item))
-        case x if x < 11 => itemQualityIncrease(item)
-        case _ => item
+  def concertSpecialQualityIncrease(
+    itemQuality: ItemQuality,
+    itemName: String,
+    itemSellIn: Int
+  ): ItemQuality =
+    if (itemQuality.value < 50 && itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      itemSellIn match {
+        case x if x < 6 => itemQualityIncrease(itemQualityIncrease(itemQuality))
+        case x if x < 11 => itemQualityIncrease(itemQuality)
+        case _ => itemQuality
       }
-    } else item
+    } else itemQuality
 
-  def sellInDecrease(item: Item): Item =
-    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-      item.sellIn = item.sellIn - 1
-      item
-    } else item
+  def sellInDecrease(itemName: String, itemSellIn: Int): ItemSellIn =
+    if (!itemName.equals("Sulfuras, Hand of Ragnaros")) {
+       ItemSellIn(itemSellIn - 1)
+    } else ItemSellIn(itemSellIn)
 
-  def itemQualityIncrease(item: Item): Item =
-    if (item.quality < 50) {
-      item.quality = item.quality + 1
-      item
-    } else item
+  def itemQualityIncrease(itemQuality: ItemQuality): ItemQuality =
+    if (itemQuality.value < 50) ItemQuality(itemQuality.value + 1)  else itemQuality
 
-  def itemQualityDecrease(item: Item) =
-    if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-      item.quality = item.quality - 1
-      item
-    } else item
+  def itemQualityDecrease(itemQuality: ItemQuality, itemName: String) : ItemQuality =
+    if (itemQuality.value > 0 && !itemName.equals("Sulfuras, Hand of Ragnaros")) {
+      ItemQuality(itemQuality.value - 1)
+    } else itemQuality
 
-  def negativeSellInItemQualityDecrease(item: Item): Item =
-    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      itemQualityDecrease(item)
-      item
+  def negativeSellInItemQualityDecrease(itemQuality: ItemQuality, itemName: String): ItemQuality =
+    if (!itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      itemQualityDecrease(itemQuality, itemName)
+    } else ItemQuality()
+
+  def negativeSellInItemQualityDecreaseOrIncrease(itemQuality: ItemQuality, itemName: String): ItemQuality =
+    if (!itemName.equals("Aged Brie")) {
+      negativeSellInItemQualityDecrease(itemQuality, itemName)
     } else {
-      item.quality = 0
-      item
+      itemQualityIncrease(itemQuality)
     }
-  def negativeSellInItemQualityDecreaseOrIncrease(item: Item): Item =
-    if (!item.name.equals("Aged Brie")) {
-      negativeSellInItemQualityDecrease(item)
-    } else {
-      itemQualityIncrease(item)
-}
 
-  def negativeSellInItemQualityAdjustment(item: Item): Item =
-    if (item.sellIn < 0) {
-      negativeSellInItemQualityDecreaseOrIncrease(item)
-      item
-    } else item
+  def negativeSellInItemQualityAdjustment(
+    itemQuality: ItemQuality,
+    itemName: String,
+    itemSellIn: ItemSellIn
+  ): ItemQuality =
+    if (itemSellIn.value < 0) {
+      negativeSellInItemQualityDecreaseOrIncrease(itemQuality, itemName)
+    } else itemQuality
 
   def updateQuality() {
     for (i <- 0 until items.length) {
-      val item1: Item = initialItemQualityIncreaseOrDecrease(items(i))
+      val itemQualityInitial: ItemQuality = initialItemQualityIncreaseOrDecrease(
+        ItemQuality(items(i).quality),
+        items(i).name,
+        items(i).sellIn
+      )
 
-      val item2: Item = sellInDecrease(item1)
+      val itemSellIn: ItemSellIn = sellInDecrease(items(i).name, items(i).sellIn)
 
-      val item3: Item = negativeSellInItemQualityAdjustment(item2)
+      val itemQuality: ItemQuality = negativeSellInItemQualityAdjustment(
+        itemQualityInitial,
+        items(i).name,
+        itemSellIn
+      )
 
-      items(i) = item3
+      items(i).quality = itemQuality.value
+      items(i).sellIn = itemSellIn.value
     }
   }
 }
--- GildedRoseKata/scala/src/main/scala/com/gildedrose/Item.scala
+++ GildedRoseKata/scala/src/main/scala/com/gildedrose/Item.scala
@@ -3,3 +3,14 @@ package com.gildedrose
 class Item(val name: String, var sellIn: Int, var quality: Int) {
 
 }
+
+case class ItemQuality(value: Int)
+case class ItemSellIn(value: Int)
+
+object ItemQuality {
+  //def apply(quality: Int): Option[ItemQuality] = ???
+  def apply(): ItemQuality = ItemQuality(0)
+  def checkRange(itemQuality: ItemQuality): Boolean = (0 until 50) contains itemQuality.value
+  def increaseQuality(itemQuality: ItemQuality): ItemQuality = ???
+  def decreaseQuality(itemQuality: ItemQuality): ItemQuality = ???
+}
