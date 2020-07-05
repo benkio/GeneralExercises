9a022109574c2cba84c46714140c845a61db309e
Enrico Benini <benkio89@gmail.com>
2020-07-05 21:48:48 +0100

    Add the logic to the classes, remove the logic from gilded rose.
    All test passes. Now we can add the new feature

--- GildedRoseKata/scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ GildedRoseKata/scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,36 +1,10 @@
 package com.gildedrose
 
 object GildedRose {
-
-  def initialItemQualityIncreaseOrDecrease(
-    itemQuality: ItemQuality,
-    itemName: ItemName,
-    itemSellIn: Int
-  ): ItemQuality = (
-    (!ItemName.isAgedBrie(itemName) && !ItemName.isBackstagePasses(itemName)),
-    ItemName.isBackstagePasses(itemName),
-    itemSellIn) match {
-    case (true, _, _) => ItemQuality.decreaseQuality(itemQuality, itemName)
-    case (false, true ,x) if x < 6 => ItemQuality.increaseQuality(ItemQuality.increaseQuality(ItemQuality.increaseQuality(itemQuality)))
-    case (false, true, x) if x < 11 => ItemQuality.increaseQuality(ItemQuality.increaseQuality(itemQuality))
-    case _ => ItemQuality.increaseQuality(itemQuality)
-  }
-
-  def negativeSellInItemQualityAdjustment(
-    itemQuality: ItemQuality,
-    itemName: ItemName,
-    itemSellIn: ItemSellIn
-  ): ItemQuality = (ItemSellIn.isExpired(itemSellIn), !ItemName.isAgedBrie(itemName), !ItemName.isBackstagePasses(itemName)) match {
-    case (true, true, true) => ItemQuality.decreaseQuality(itemQuality, itemName)
-    case (true, true, false) => ItemQuality()
-    case (true, false, _) =>  ItemQuality.increaseQuality(itemQuality)
-    case (false, _, _) => itemQuality
-  }
-
   def updateQuality(items: Array[Item]): Array[Item] = for {
-      item <- items
-      initialQuality = initialItemQualityIncreaseOrDecrease(ItemQuality(item.quality), item.name, item.sellIn)
-      itemSellIn = ItemSellIn.decrease(ItemSellIn(item.sellIn), item.name)
-      resultQuality = negativeSellInItemQualityAdjustment(initialQuality, item.name, itemSellIn)
-    } yield new Item(item.name, itemSellIn.value, resultQuality.value)
+    itemRaw <- items
+  } yield Item.refine(itemRaw)
+    .preSellInQualityAdjustment()
+    .sellInDecrease()
+    .postSellInQualityAdjustment()
 }
--- GildedRoseKata/scala/src/main/scala/com/gildedrose/Item.scala
+++ GildedRoseKata/scala/src/main/scala/com/gildedrose/Item.scala
@@ -1,28 +1,34 @@
 package com.gildedrose
 
-class Item(val name: String, var sellIn: Int, var quality: Int){
+class Item(val name: String, var sellIn: Int, var quality: Int) {
 
-  def preSellInQualityAdjustment(): ItemQuality = ItemQuality.decreaseQuality(
-    ItemQuality(quality),
-    name
+  def preSellInQualityAdjustment(): Item = copy(
+    quality = ItemQuality.decreaseQuality(
+      ItemQuality(quality)
+    ).value)
+  def postSellInQualityAdjustment(): Item = if (ItemSellIn.isExpired(ItemSellIn(sellIn))) copy(quality = ItemQuality.decreaseQuality(ItemQuality(quality)).value)
+  else this
+  def sellInDecrease(): Item = copy(sellIn = ItemSellIn(sellIn - 1).value)
+
+  //Can't touch the Item definition so I manually add the copy method
+  def copy(sellIn: Int = sellIn, quality: Int = quality): Item = new Item(
+    this.name,
+    sellIn,
+    quality
   )
-  def postSellInQualityAdjustment(): ItemQuality =
-    if (ItemSellIn.isExpired(ItemSellIn(sellIn))) ItemQuality.decreaseQuality(ItemQuality(quality), name)
-    else ItemQuality(quality)
 }
 
 case class ItemQuality(value: Int) extends AnyVal
 case class ItemSellIn(value: Int) extends AnyVal
 
 object ItemQuality {
-  //def apply(quality: Int): Option[ItemQuality] = ???
   def apply(): ItemQuality = ItemQuality(0)
   def checkRange(itemQuality: ItemQuality): Boolean = (0 until 50) contains itemQuality.value
   def increaseQuality(itemQuality: ItemQuality): ItemQuality =
     if (checkRange(itemQuality)) ItemQuality(itemQuality.value + 1) else itemQuality
-  def decreaseQuality(itemQuality: ItemQuality, itemName: ItemName) : ItemQuality = {
+  def decreaseQuality(itemQuality: ItemQuality) : ItemQuality = {
     val newQuality = ItemQuality(itemQuality.value - 1)
-    if (checkRange(newQuality) && !ItemName.isSulfuras(itemName)) newQuality else itemQuality
+    if (checkRange(newQuality)) newQuality else itemQuality
   }
 }
 
@@ -34,34 +40,44 @@ object ItemName {
 
 object ItemSellIn {
   def isExpired(itemSellIn: ItemSellIn): Boolean = itemSellIn.value < 0
-  def decrease(itemSellIn: ItemSellIn, itemName: ItemName): ItemSellIn =
-    if (!ItemName.isSulfuras(itemName)) {
-      ItemSellIn(itemSellIn.value - 1)
-    } else itemSellIn
 }
 
 class Sulfuras(sellIn: Int, quality: Int) extends Item(Sulfuras.name, sellIn, quality) {
-  override def preSellInQualityAdjustment(): ItemQuality = ItemQuality(quality)
-  override def postSellInQualityAdjustment(): ItemQuality = ItemQuality(quality)
+  override def preSellInQualityAdjustment(): Item = this
+  override def postSellInQualityAdjustment(): Item = this
+  override def sellInDecrease(): Item = this
 }
 object Sulfuras {
   val name: ItemName = "Sulfuras, Hand of Ragnaros"
 }
 class BackstagePasses(sellIn: Int, quality: Int) extends Item(BackstagePasses.name, sellIn, quality) {
-  override def preSellInQualityAdjustment(): ItemQuality  = sellIn match {
-    case x if x < 6 => ItemQuality.increaseQuality(ItemQuality.increaseQuality(ItemQuality.increaseQuality(ItemQuality(quality))))
-    case x if x < 11 => ItemQuality.increaseQuality(ItemQuality.increaseQuality(ItemQuality(quality)))
-    case _ => ItemQuality.increaseQuality(ItemQuality(quality))
+  override def preSellInQualityAdjustment(): Item  = {
+    val newQuality = sellIn match {
+      case x if x < 6 => ItemQuality.increaseQuality(ItemQuality.increaseQuality(ItemQuality.increaseQuality(ItemQuality(quality))))
+      case x if x < 11 => ItemQuality.increaseQuality(ItemQuality.increaseQuality(ItemQuality(quality)))
+      case _ => ItemQuality.increaseQuality(ItemQuality(quality))
+    }
+    this.copy(quality = newQuality.value)
   }
-  override def postSellInQualityAdjustment(): ItemQuality = ItemQuality()
+  override def postSellInQualityAdjustment(): Item = this.copy(quality = ItemQuality().value)
 }
 object BackstagePasses {
   val name: ItemName = "Backstage passes to a TAFKAL80ETC concert"
 }
 class AgedBrie(sellIn: Int, quality: Int) extends Item(AgedBrie.name, sellIn, quality) {
-  override def preSellInQualityAdjustment(): ItemQuality  = ItemQuality.increaseQuality(ItemQuality(quality))
-  override def postSellInQualityAdjustment(): ItemQuality = ItemQuality.increaseQuality(ItemQuality(quality))
+  override def preSellInQualityAdjustment(): Item  = this.copy(quality = ItemQuality.increaseQuality(ItemQuality(quality)).value)
+  override def postSellInQualityAdjustment(): Item = this.copy(quality = ItemQuality.increaseQuality(ItemQuality(quality)).value)
 }
 object AgedBrie {
   val name: ItemName = "Aged Brie"
+}
+
+object Item {
+
+  def refine(item: Item): Item = item.name match {
+    case Sulfuras.name         => new Sulfuras(item.sellIn, item.quality)
+    case BackstagePasses.name  => new BackstagePasses(item.sellIn, item.quality)
+    case AgedBrie.name         => new AgedBrie(item.sellIn, item.quality)
+    case _ => item
+  }
 }
