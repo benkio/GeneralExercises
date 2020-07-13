d9be7922c3ded0e83b506a51751e65a510574baa
Enrico Benini <ebenini@mdsol.com>
2020-07-13 11:16:47 +0100

    adding more tests

--- GildedRoseKata/haskell/test/BackstagePassesSpec.hs
+++ GildedRoseKata/haskell/test/BackstagePassesSpec.hs
@@ -11,3 +11,10 @@ spec = describe "BackstagePassesSpec" $ do
   describe "should increase in quality" $ do
     it "by one if the expiration date is far away (> 10)" $ property $ do
       forAll (backstagePassesFarGen) $ qualityCheck ascendingQualty1
+    it "by two if the expiration date is close (5-10)" $ property $ do
+      forAll (backstagePassesCloseGen) $ qualityCheck ascendingQualty2
+    it "by three if the expiration date is closest (0-5)" $ property $ do
+      forAll (backstagePassesCloseGen) $ qualityCheck ascendingQualty3
+  describe "should have quality == 0" $ do
+    it "When it expires" $ do
+      pending
--- GildedRoseKata/haskell/test/SuccessConditions.hs
+++ GildedRoseKata/haskell/test/SuccessConditions.hs
@@ -1,5 +1,16 @@
 module SuccessConditions where
 
+ascendingQualty :: Int -> (Int, Int) -> Bool
+ascendingQualty distance (oldQ, newQ) =
+  let upperBoundExceptions = [(50 - distance + 1) .. 50]
+      exceptionConditions = foldl (\acc x -> acc || (oldQ == x && newQ == 50)) True upperBoundExceptions
+  in oldQ == (newQ - distance) || exceptionConditions
+
 ascendingQualty1 :: (Int, Int) -> Bool
-ascendingQualty1 (oldQ, newQ) =
-  oldQ < newQ || (oldQ == 50 && newQ == 50)
+ascendingQualty1 = ascendingQualty 1
+
+ascendingQualty2 :: (Int, Int) ->  Bool
+ascendingQualty2 = ascendingQualty 2
+
+ascendingQualty3 :: (Int, Int) -> Bool
+ascendingQualty3 = ascendingQualty 3
