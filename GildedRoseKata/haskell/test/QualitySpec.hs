35d78a5a2af3f1fefe1e5b786876b08e01c8e84e
Enrico Benini <ebenini@mdsol.com>
2020-07-14 09:22:23 +0100

    Add Quality Spec skeleton

---
+++ GildedRoseKata/haskell/test/QualitySpec.hs
@@ -0,0 +1,15 @@
+module QualitySpec (spec) where
+
+--import Generators
+import Test.Hspec
+--import Test.QuickCheck
+
+spec :: Spec
+spec = describe "QualitySpec" $ do
+  it "Quality should never be negative" $ do
+    pending
+  it "Quality should never be > 50" $ do
+    pending
+  it "Once the sell by date has passed, Quality degrades twice as fast" $ do
+    pending
+  
