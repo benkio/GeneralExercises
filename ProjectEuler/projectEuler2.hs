06304a1ed5fd9c04e7c318c4c255c948c1a8bbb2
Enrico Benini <ebenini@mdsol.com>
2020-08-19 19:51:53 +0100

    es 18

--- ProjectEuler/projectEuler2.hs
+++ ProjectEuler/projectEuler2.hs
@@ -31,8 +31,11 @@ inputGrid =
  \ 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 \n\
  \ 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
 
+readIntLines :: String -> [[Int]]
+readIntLines = (fmap (fmap (\x -> read x :: Int) . words) . lines)
+
 horizontal :: [[Int]]
-horizontal = (fmap (fmap (\x -> read x :: Int) . words) . lines) inputGrid
+horizontal = readIntLines inputGrid
 
 vertical :: [[Int]]
 vertical = transpose horizontal
@@ -315,3 +318,33 @@ oneThousandNumbersToChar = mapM_ (putStrLn . fourDigitStringCount) [1..1000]
 
 oneThousandNumbersToCharCount :: Int
 oneThousandNumbersToCharCount = sum $ fmap (length . filter (not . (`elem` " -")) . fourDigitStringCount) [1..1000]
+
+-- Es 18 --------------------------------------
+
+inputTriangle :: String
+inputTriangle = "75                                           \n\
+              \  95 64                                        \n\
+              \  17 47 82                                     \n\
+              \  18 35 87 10                                  \n\
+              \  20 04 82 47 65                               \n\
+              \  19 01 23 75 03 34                            \n\
+              \  88 02 77 73 07 63 67                         \n\
+              \  99 65 04 28 06 16 70 92                      \n\
+              \  41 41 26 56 83 40 80 70 33                   \n\
+              \  41 48 72 33 47 32 37 16 94 29                \n\
+              \  53 71 44 65 25 43 91 52 97 51 14             \n\
+              \  70 11 33 28 77 73 17 78 39 68 17 57          \n\
+              \  91 71 52 38 17 14 91 43 58 50 27 29 48       \n\
+              \  63 66 04 68 89 53 67 30 73 16 69 87 40 31    \n\
+              \  04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
+
+triangle :: [[Int]]
+triangle = readIntLines inputTriangle
+
+maxPath :: Int
+maxPath = sum $ foldr collapseTriangle (replicate 16 0) triangle
+  where
+    collapseTriangle :: [Int] -> [Int] -> [Int]
+    collapseTriangle ts acc =
+      let maxacc = (fmap (uncurry max). zip (tail acc)) acc
+      in fmap (uncurry (+)) $ maxacc `zip` ts
