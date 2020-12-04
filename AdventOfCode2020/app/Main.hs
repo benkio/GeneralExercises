0dfa326740fdb8a0c82610e5526c2a2681a2f753
Enrico Benini <benkio89@gmail.com>
2020-12-04 17:56:36 +0000

    Advent of code day 4

--- AdventOfCode2020/app/Main.hs
+++ AdventOfCode2020/app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified FirstDecember as First
 import qualified SecondDecember as Second
 import qualified ThirdDecember as Third
+import qualified ForthDecember as Forth
 
 main :: IO ()
-main = Third.thirdDecemberSolution2 >>= print
+main = Forth.forthDecemberSolution1 >>= print
