cda31b9c7b34d7a950a7e9537ebc94dd003f1306
benkio <benkio89@gmail.com>
2020-12-05 23:19:16 +0000

    Advent of Code day 5

--- AdventOfCode2020/app/Main.hs
+++ AdventOfCode2020/app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
+import qualified FifthDecember  as Fifth
 import qualified FirstDecember  as First
 import qualified FourthDecember as Fourth
 import qualified SecondDecember as Second
 import qualified ThirdDecember  as Third
 
 main :: IO ()
-main = Fourth.fourthDecemberSolution1 >>= print
+main = Fifth.fifthDecemberSolution2 >>= print
