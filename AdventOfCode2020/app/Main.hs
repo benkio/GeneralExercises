8fba1c1ea69e25d4b7d255a5171bd701dfbf9855
benkio <benkio89@gmail.com>
2020-12-06 14:25:13 +0000

    Advent of Code day 6

--- AdventOfCode2020/app/Main.hs
+++ AdventOfCode2020/app/Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
-import qualified FifthDecember as Fifth
-import qualified FirstDecember as First
+import qualified FifthDecember  as Fifth
+import qualified FirstDecember  as First
 import qualified FourthDecember as Fourth
 import qualified SecondDecember as Second
-import qualified ThirdDecember as Third
+import qualified SixthDecember  as Sixth
+import qualified ThirdDecember  as Third
 
 main :: IO ()
-main = Fifth.fifthDecemberSolution2 >>= print
+main = Sixth.sixthDecemberSolution2 >>= print
--- AdventOfCode2020/src/SixthDecember.hs
+++ AdventOfCode2020/src/SixthDecember.hs
@@ -3,6 +3,26 @@
 -------------------------------------------------------------------------------
 module SixthDecember where
 
+import Data.List (groupBy, intersect, nub)
 
-input :: IO String
-input = readFile "input/6December.txt"
+questionIds :: [Char]
+questionIds = ['a' .. 'z']
+
+splitByGroup :: [String] -> [[String]]
+splitByGroup = fmap (filter ("" /=)) . groupBy (\_ x -> x /= "")
+
+countTotalAnyoneYesAnswers :: [[String]] -> Int
+countTotalAnyoneYesAnswers = sum . fmap (length . nub . concat)
+
+countTotalEveryoneYesAnswers :: [[String]] -> Int
+countTotalEveryoneYesAnswers =
+  sum . fmap (length . foldl1 intersect) . filter (not . null)
+
+input :: IO [[String]]
+input = splitByGroup . lines <$> readFile "input/6December.txt"
+
+sixthDecemberSolution1 :: IO Int
+sixthDecemberSolution1 = countTotalAnyoneYesAnswers <$> input
+
+sixthDecemberSolution2 :: IO Int
+sixthDecemberSolution2 = countTotalEveryoneYesAnswers <$> input
