3366fc83f7579da07ed9f4d9c215d5f2d8fd5aa0
benkio <benkio89@gmail.com>
2020-12-09 12:17:10 +0000

    Advent of code day 9

--- AdventOfCode2020/app/Main.hs
+++ AdventOfCode2020/app/Main.hs
@@ -4,10 +4,11 @@ import qualified EighthDecember  as Eighth
 import qualified FifthDecember   as Fifth
 import qualified FirstDecember   as First
 import qualified FourthDecember  as Fourth
+import qualified NinthDecember   as Ninth
 import qualified SecondDecember  as Second
 import qualified SeventhDecember as Seventh
 import qualified SixthDecember   as Sixth
 import qualified ThirdDecember   as Third
 
 main :: IO ()
-main = Eighth.eighthDecemberSolution2 >>= print
+main = Ninth.ninthDecemberSolution2 >>= print
