22caf00b4cf446f6763f7a652bc7ec04fd106bc5
benkio <benkio89@gmail.com>
2020-12-22 03:09:07 +0100

    Restart the part 2 with a different approach. looking to change part 1

--- AdventOfCode/src/TwentyTwenty/TwentiethDecember.hs
+++ AdventOfCode/src/TwentyTwenty/TwentiethDecember.hs
@@ -3,23 +3,25 @@
 -------------------------------------------------------------------------------
 module TwentyTwenty.TwentiethDecember where
 
-import           Data.Bifunctor (second)
-import           Data.List      (delete, find, group, groupBy, partition, sort,
-                                 transpose)
-import           Data.Map       (Map, difference, empty, fromList, insert,
-                                 mapKeys, toList, union)
-import           Data.Maybe     (fromJust, isJust)
+import           Data.Bifunctor (bimap, first, second)
+import           Data.List      (delete, find, group, groupBy, maximumBy,
+                                 partition, sort, transpose, (\\))
+import qualified Data.Map       as M (Map, delete, difference, empty, filter,
+                                      foldrWithKey, fromList, insert,
+                                      intersection, keys, keysSet, lookup,
+                                      mapKeys, toList, union, withoutKeys)
+import           Data.Maybe     (catMaybes, fromJust, isJust)
 
 type Coordinate = (Int, Int)
 
 data Tile =
-  Tile Int (Map Coordinate Bool)
+  Tile Int (M.Map Coordinate Bool)
   deriving (Show)
 
 tileId :: Tile -> Int
 tileId (Tile i _) = i
 
-tileContent :: Tile -> (Map Coordinate Bool)
+tileContent :: Tile -> M.Map Coordinate Bool
 tileContent (Tile _ c) = c
 
 input :: IO String
@@ -32,7 +34,7 @@ parseTiles s = do
           s
   rawTile <- rawTiles
   let tId = ((\x -> read x :: Int) . init . dropWhile (' ' /=)) $ head rawTile
-  (fmap (foldl1 (\(Tile x m1) (Tile _ m2) -> Tile x (m1 `union` m2))) .
+  (fmap (foldl1 (\(Tile x m1) (Tile _ m2) -> Tile x (m1 `M.union` m2))) .
    groupBy (\t1 t2 -> tileId t1 == tileId t2) .
    fmap
      (\(y, s') ->
@@ -41,19 +43,19 @@ parseTiles s = do
           (foldl
              (\m (x, c) ->
                 if c == '#'
-                  then insert (x, y) True m
-                  else insert (x, y) False m)
-             empty
+                  then M.insert (x, y) True m
+                  else M.insert (x, y) False m)
+             M.empty
              s')) .
    fmap (second (zip [0 ..])) . zip [0 ..] . tail)
     rawTile
 
-tileEdges :: (Map Coordinate Bool) -> [[(Coordinate, Bool)]]
+tileEdges :: M.Map Coordinate Bool -> [[(Coordinate, Bool)]]
 tileEdges t1 =
   let (t1EdgesWest, t1EdgesEst) =
-        ((\l -> (take 10 l, drop (length l - 10) l)) . toList) t1
+        ((\l -> (take 10 l, drop (length l - 10) l)) . M.toList) t1
       (t1EdgesSouth, t1EdgesNorth) =
-        ((\l -> (fmap (l !!) [9,19 .. 99], fmap (l !!) [0,10 .. 90])) . toList)
+        ((\l -> (fmap (l !!) [9,19 .. 99], fmap (l !!) [0,10 .. 90])) . M.toList)
           t1
    in [t1EdgesWest, t1EdgesSouth, t1EdgesEst, t1EdgesNorth]
 
@@ -77,19 +79,20 @@ removeDuplicateEdges ::
      ([(Int, [[(Coordinate, Bool)]])], [(Int, [[(Coordinate, Bool)]])])
   -> (Int, [[(Coordinate, Bool)]])
   -> ([(Int, [[(Coordinate, Bool)]])], [(Int, [[(Coordinate, Bool)]])])
-removeDuplicateEdges (edgesToCheck, standaloneEdges) (tId, edges) =
+removeDuplicateEdges (resultEdgeToCheck, standaloneEdges) (tId, resultEdge) =
   let (matches, notMatches) =
         partition
-          (\x -> fmap snd x `elem` (edgesToCheck >>= (snd . stripCoordinates)))
-          edges
-      edgesToCheckNoDuplicates =
+          (\x ->
+             fmap snd x `elem` (resultEdgeToCheck >>= (snd . stripCoordinates)))
+          resultEdge
+      resultEdgeToCheckNoDuplicates =
         second
           (filter
              (\x ->
                 fmap snd x `notElem`
                 (fmap . fmap) snd (matches ++ fmap reverse matches))) <$>
-        edgesToCheck
-   in (edgesToCheckNoDuplicates, standaloneEdges ++ [(tId, notMatches)])
+        resultEdgeToCheck
+   in (resultEdgeToCheckNoDuplicates, standaloneEdges ++ [(tId, notMatches)])
 
 showRow :: [Bool] -> String
 showRow =
@@ -106,115 +109,133 @@ showTile (Tile tId content) =
   show tId ++
   ":\n" ++
   (foldl (\s r -> s ++ showRow (fmap snd r) ++ "\n") "" .
-   transpose . groupBy (\((x, _), _) ((x', _), _) -> x == x') . toList)
+   transpose . groupBy (\((x, _), _) ((x', _), _) -> x == x') . M.toList)
     content
 
 solution1 :: String -> Int
 solution1 = product . fmap fst . cornerTiles
 
-cornerTiles :: String -> [(Int, Coordinate)]
+cornerTiles :: String -> [(Int, [[(Coordinate, Bool)]])]
 cornerTiles s =
   let tiles = parseTiles s
-      edges =
+      resultEdge =
         addReverseEdges . (\t -> (tileId t, tileEdges (tileContent t))) <$>
         tiles
       corners =
-        (fmap (second findCorner) .
-         filter ((2 <) . length . snd) . (`removeDuplicatesEdgesInTailes` []))
-          edges
+        (filter ((2 <) . length . snd) . (`removeDuplicatesEdgesInTailes` []))
+          resultEdge
    in corners
-
-findCorner :: [[(Coordinate, Bool)]] -> Coordinate
-findCorner xs =
-  (fst . head . head . filter ((== length xs) . length) . group . sort . concat)
-    xs
-
-adjustNewOrigin :: Map Coordinate Bool -> Coordinate -> Map Coordinate Bool
-adjustNewOrigin content (x, y)
-  | x == 0 && y == 0 = content
-  | otherwise = mapKeys (\(x', y') -> (abs (x' - x), abs (y' - y))) content
 
 twentiethDecemberSolution1 :: IO Int
 twentiethDecemberSolution1 = solution1 <$> input
 
-solution2 s =
-  let tiles = parseTiles s
-      selectedCorner = (head . cornerTiles) s
-      startingCorner =
-        (fromJust .
-         (\(tId, corner) ->
-            (fmap ((`adjustNewOrigin` corner) . tileContent) .
-             find ((tId ==) . tileId))
-              tiles))
-          selectedCorner
-      bigMosaic =
-        linkTiles
-          ((fmap tileContent . tail) tiles)
-          startingCorner
-          ((filter (\e -> snd selectedCorner `notElem` fmap fst e) . tileEdges)
-             startingCorner)
-   in showTile $ Tile 0 bigMosaic
+-- Solution2 -------------------------------------
+type TileId = Int
 
-linkTiles ::
-     [Map Coordinate Bool]
-  -> Map Coordinate Bool
-  -> [[(Coordinate, Bool)]]
-  -> Map Coordinate Bool
-linkTiles [] result resultEdges =
-  let externalEdges = (fromList . concat) resultEdges
-   in difference result externalEdges
-linkTiles (t:ts) result resultEdges =
-  let maybeMatchResultNEdges = searchMatch resultEdges t
-   in foldr
-        (\(tileToAdd, matchingEdge) _ ->
-           let result' = mergeTile result tileToAdd matchingEdge
-               resultEdges' =
-                 delete matchingEdge resultEdges ++
-                 delete matchingEdge (tileEdges t)
-            in linkTiles ts result' resultEdges')
-        (linkTiles (ts ++ [t]) result resultEdges)
-        maybeMatchResultNEdges
+type Row = [(Coordinate, Bool)]
 
-searchMatch ::
-     [[(Coordinate, Bool)]]
-  -> Map Coordinate Bool
-  -> Maybe (Map Coordinate Bool, [(Coordinate, Bool)])
-searchMatch resultEdges t =
-  let tEdges = tileEdges t
-   in foldl
-        (\acc x ->
-           if isJust acc
-             then acc
-             else matchEdge x resultEdges t)
-        Nothing
-        tEdges
+data Direction
+  = W
+  | S
+  | E
+  | N
+  | Self
+  deriving (Eq, Show, Ord)
 
--- The tile rotates the big result doesn't move
-matchEdge ::
-     [(Coordinate, Bool)]
-  -> [[(Coordinate, Bool)]]
-  -> Map Coordinate Bool
-  -> Maybe (Map Coordinate Bool, [(Coordinate, Bool)])
-matchEdge edge resultEdges t
-  | fmap snd edge `elem` (fmap . fmap) snd resultEdges = Just (t, edge)
-  | (reverse . fmap snd) edge `elem` (fmap . fmap) snd resultEdges =
-    Just (adjustNewOrigin t (maximum (fmap fst edge)), edge)
-  | otherwise = Nothing
+type TileDb = M.Map (TileId, Direction) Tile
 
--- merge only left and down since I use a corner in left top position as starting point
-mergeTile ::
-     Map Coordinate Bool
-  -> Map Coordinate Bool
-  -> [(Coordinate, Bool)]
-  -> Map Coordinate Bool
-mergeTile result tileToAdd commonEdge = undefined
-  -- let edges = fromList commonEdge
-  --     resultEdge = (toList . intersection result) edges
+rotateUp :: Tile -> Tile
+rotateUp (Tile tId content) =
+  Tile tId $ M.mapKeys (\(x, y) -> (abs (y - 9), x)) content
 
+flipTile :: Tile -> Tile
+flipTile (Tile tId content) =
+  Tile tId $ M.mapKeys (\(x, y) -> (abs (x - 9), y)) content
 
-edgeIsVertical :: [(Coordinate, Bool)] -> Bool
-edgeIsVertical = (\e -> all (head e ==) (tail e)) . fmap fst
+directionToInt :: Direction -> Int
+directionToInt W = 0
+directionToInt S = 1
+directionToInt E = 2
+directionToInt N = 3
 
+directions :: [Direction]
+directions = [W, S, E, N]
+
+getEdges :: Tile -> [Direction] -> [(Direction, Row)]
+getEdges (Tile _ tc) = fmap (\d -> (d, tileEdges tc !! directionToInt d))
+
+howManyOpenEdges :: Tile -> TileDb -> [Direction]
+howManyOpenEdges (Tile tId _) =
+  (directions \\) . fmap snd . M.keys . M.filter ((tId ==) . tileId)
+
+findNeighbor :: [Tile] -> (Direction, Row) -> Maybe (Tile, Direction)
+findNeighbor [] _ = Nothing
+findNeighbor (t:ts) edge =
+  let maybeNeighbor = isNeighbor edge t
+  in if isJust maybeNeighbor then maybeNeighbor else findNeighbor ts edge
+
+isNeighbor :: (Direction, Row) -> Tile -> Maybe (Tile, Direction)
+isNeighbor (edgeDirection, edge) t =
+  let tileConfigurations = take 4 (iterate rotateUp t) >>= \t' -> t':[flipTile t']
+      matchTile = find (\t' -> edge == (snd . head) (getEdges t' [edgeDirection])) tileConfigurations
+   in (\t' -> (t', edgeDirection)) <$> matchTile
+
+updateTileDb :: TileId -> TileDb -> [(Tile, Direction)] -> TileDb
+updateTileDb _ tDb [] = tDb
+updateTileDb tId tDb ((n, nd):ns) =
+  let tDb' = M.insert (tId, nd) n tDb
+      tDb'' = if isJust (M.lookup (tileId n, Self) tDb')
+        then tDb'
+        else M.insert (tileId n, Self) n tDb'
+  in updateTileDb tId tDb'' ns
+
+getCorners :: TileDb -> [Tile]
+getCorners tDb =
+  let cornersId = (fmap head . filter ((4==) . length) . group . sort . M.foldrWithKey (\(tId,_) (Tile tId' _) acc -> tId:tId':acc) []) tDb
+  in (\tId -> fromJust (M.lookup (tId, Self) tDb) ) <$> cornersId
+
+buildImage :: TileDb -> Tile
+buildImage tdb = undefined
+
+buildTileDatabase :: [Tile] -> TileDb -> TileDb
+buildTileDatabase [] tDb = tDb
+buildTileDatabase (t:ts) tDb =
+  let
+    maybeTileSelf = M.lookup (tileId t, Self) tDb
+    tileSelf = foldr const t maybeTileSelf
+    openEdges = getEdges tileSelf $ howManyOpenEdges tileSelf tDb
+    neighboors =
+        catMaybes $
+        foldl (\acc edge -> acc ++ [findNeighbor ts edge]) [] openEdges
+    newTDb = updateTileDb (tileId t) tDb neighboors
+    newTDb' = if isJust maybeTileSelf
+      then newTDb
+      else M.insert (tileId tileSelf, Self) tileSelf newTDb
+   in buildTileDatabase ts newTDb'
+
+seaMonster :: Tile
+seaMonster = undefined
+
+searchSeaMonster :: Tile -> [(Tile, Int)]
+searchSeaMonster image = undefined
+
+killSeaMonsters :: Tile -> Tile
+killSeaMonsters image = undefined
+
+countRoughWater :: Tile -> Int
+countRoughWater t = undefined
+
+solution2 :: [Tile] -> Int
+solution2 ts =
+  let tileDatabase = buildTileDatabase ts M.empty
+      image = buildImage tileDatabase
+      mostSeaMonsterConfiguration =
+        fst . maximumBy (\(_, m) (_, m') -> m `compare` m') $
+        searchSeaMonster image
+      waters = killSeaMonsters mostSeaMonsterConfiguration
+   in countRoughWater waters
+
+-- test datat ------------------------------------------------------------
 testInput :: String
 testInput =
   "Tile 2311:\n\
