cbef954416c16bbad984a48ff830167b52d04ce0
benkio <benkio89@gmail.com>
2021-01-04 21:26:09 +0100

    Advent of Code Day 6 2015

--- AdventOfCode/app/Main.hs
+++ AdventOfCode/app/Main.hs
@@ -29,7 +29,8 @@ module Main where
 -- import qualified TwentyFifteen.SecondDecember as Second
 -- import qualified TwentyFifteen.ThirdDecember as Third
 --import qualified TwentyFifteen.FourthDecember as Four
-import qualified TwentyFifteen.FifthDecember as Five
+--import qualified TwentyFifteen.FifthDecember as Five
+import qualified TwentyFifteen.SixthDecember as Six
 
 main :: IO ()
-main = Five.fifthDecemberSolution2 >>= print
+main = Six.sixthDecemberSolution2 >>= print
