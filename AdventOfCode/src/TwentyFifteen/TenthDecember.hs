ce6cc9a0c67ca7b4a15371158e72534c3e5bacf7
benkio <benkio89@gmail.com>
2021-01-09 16:04:25 +0100

    hlint

--- AdventOfCode/src/TwentyFifteen/TenthDecember.hs
+++ AdventOfCode/src/TwentyFifteen/TenthDecember.hs
@@ -1,6 +1,6 @@
 module TwentyFifteen.TenthDecember where
 
-import Data.List (group)
+import           Data.List (group)
 
 input :: IO Int
 input = read <$> readFile "input/2015/10December.txt"
@@ -9,7 +9,7 @@ inputTest :: Integer
 inputTest = 1
 
 generateLookAndSay :: Integer -> [Integer]
-generateLookAndSay x = iterate generateNextLookAndSay x
+generateLookAndSay = iterate generateNextLookAndSay
 
 generateNextLookAndSay :: Integer -> Integer
 generateNextLookAndSay =
@@ -19,8 +19,7 @@ solution1Test :: Bool
 solution1Test = (== 312211) $ solution 5 1
 
 solution :: Int -> Int -> Integer
-solution iterations initialValue =
-  ((!! iterations) . generateLookAndSay . toInteger) initialValue
+solution iterations = (!! iterations) . generateLookAndSay . toInteger
 
 tenthDecemberSolution1 :: IO Int
 tenthDecemberSolution1 = length . show . solution 40 <$> input
