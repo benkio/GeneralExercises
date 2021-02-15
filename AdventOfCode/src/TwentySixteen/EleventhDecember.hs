c62900c5de7aa0200955396dd5a1bdf730ca034c
benkio <benkio89@gmail.com>
2021-02-15 03:22:14 +0100

    Reached a solution, suboptimal

--- AdventOfCode/src/TwentySixteen/EleventhDecember.hs
+++ AdventOfCode/src/TwentySixteen/EleventhDecember.hs
@@ -2,155 +2,33 @@
 
 module TwentySixteen.EleventhDecember where
 
+import           Control.Concurrent
 import           Data.List
-import           Data.Map   (Map, elems, fromList, toList)
-import qualified Data.Map   as Map (insert, lookup)
+import           Data.Map           (Map, adjust, elems, fromList,
+                                     takeWhileAntitone, toList)
+import qualified Data.Map           as Map (lookup)
 import           Data.Maybe
 import           Data.Ord
+import           Data.Set           (Set, empty)
+import qualified Data.Set           as Set (insert)
 
 data RTG
   = Microchip String
   | Generator String
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 type Floors = Map Int [RTG]
 
 type Floor = [RTG]
-data Elevator =
-  Elevator
-    { efloor :: Int
-    , load   :: Elevatorload
+
+data State =
+  State
+    { elevatorFloor :: Int
+    , floors        :: Floors
     }
-  deriving (Eq, Show)
+  deriving (Show)
 
-data State = State { elevator :: Elevator, floors:: Floors, step:: Int, history :: [Floors]} deriving Show
-data Elevatorload
-  = Elevatorload RTG
-  | ElevatorloadUnsafe RTG RTG -- Read-only
-  deriving (Show, Eq)
-
-floorsEquality :: Floors -> Floors -> Bool
-floorsEquality fs1 fs2 =
-  all (null . uncurry (\\)) $ elems fs1 `zip` elems fs2
-
-elevatorloadToList :: Elevatorload -> [RTG]
-elevatorloadToList (Elevatorload r)          = [r]
-elevatorloadToList (ElevatorloadUnsafe r r') = [r, r']
-
-removeLoadFromFloor :: Floor -> Elevatorload -> Floor
-removeLoadFromFloor f l = f \\ elevatorloadToList l
-
-isMicrochip :: RTG -> Bool
-isMicrochip (Microchip _) = True
-isMicrochip _             = False
-
-validateRTG :: RTG -> RTG -> Bool
-validateRTG (Microchip _) (Microchip _)  = True
-validateRTG (Generator _) (Generator _)  = True
-validateRTG (Microchip s) (Generator s') = s == s'
-validateRTG (Generator s) (Microchip s') = s == s'
-
-unloadElevator :: Elevator -> Floors -> Floors
-unloadElevator e fls = Map.insert (efloor e) (unloadElevatorload (efloor e) (load e) fls) fls
-
-unloadElevatorload :: Int -> Elevatorload -> Floors -> Floor
-unloadElevatorload f l floors = elevatorloadToList l ++ (fromJust . Map.lookup f) floors
-
-validateFloor :: Int -> Elevatorload -> Floors -> Bool
-validateFloor f l floors =
-  validateFloor' $ unloadElevatorload f l floors
-
-validateFloor' :: [RTG] -> Bool
-validateFloor' fls = (all (\rtg -> validate rtg fls) . filter isMicrochip) fls
-  where
-    validate :: RTG -> [RTG] -> Bool
-    validate r@(Microchip _) rs =
-      let generators = filter (not . isMicrochip) rs
-       in null generators || any (validateRTG r) generators
-    validate r@(Generator _) rs =
-      let chips = filter isMicrochip rs
-       in null chips || any (validateRTG r) chips
-
-buildElevator :: RTG -> RTG -> Maybe Elevatorload
-buildElevator r r'
-  | validateRTG r r' = Just $ ElevatorloadUnsafe r r'
-  | otherwise = Nothing
-
-validLoads :: Floor -> [Elevatorload]
-validLoads fls =
-  ( mapMaybe
-    (\l ->
-       if length l == 1
-         then Just (Elevatorload (head l))
-         else buildElevator (head l) (last l))
-  . filter (\l -> ((`elem` [1, 2]) . length) l && validateFloor' (fls \\ l))
-  . subsequences) fls
-
-endCondition :: Elevator -> Floors -> Bool
-endCondition e fs =
-  efloor e == 4 &&
-  ((\l -> (null . concat . init) l && (not . null . last) l) . fmap snd . toList)
-    fs
-
-nextSteps :: Elevator -> Floors -> [Floors] -> [(Elevator, Floors)]
-nextSteps Elevator {efloor = f, load = l} floors his =
-  let (previousFloor, currentFloor, nextFloor) =
-        (max (f - 1) 1, fromJust (Map.lookup f floors), min (f + 1) 4)
-      floorContent = currentFloor ++ elevatorloadToList l
-      nextValidLoads =
-        (sortOn (Data.Ord.Down . length . elevatorloadToList) . validLoads)
-          floorContent
-      buildNextSteps =
-        \floorValue el ->
-          ( Elevator {efloor = floorValue, load = el}
-          , Map.insert f (removeLoadFromFloor floorContent el) floors)
-
-      nextLoadUp =
-        ( filter (not . (\x -> any (`floorsEquality` x) his) . uncurry unloadElevator)
-          . fmap (buildNextSteps nextFloor)
-          . filter (\el -> nextFloor /= f && validateFloor nextFloor el floors)) nextValidLoads
-      nextLoadDown =
-        ( filter (not . (\x -> any (`floorsEquality` x) his) . uncurry unloadElevator)
-          . fmap (buildNextSteps previousFloor)
-          . filter (\el -> previousFloor /= f && validateFloor previousFloor el floors)
-          . reverse
-        )
-        nextValidLoads
-   in if null nextLoadUp then nextLoadDown else nextLoadUp
-
-higherFloorIndex :: Elevator -> Floors -> Int
-higherFloorIndex Elevator { efloor = f, load = el} fls =
-  ((f *) . length . elevatorloadToList) el +
-  (sum . fmap (\(i,l) -> length l * i) . toList) fls
-
-stepZero :: Floors -> State
-stepZero fls =
-  let firstFloor = (fromJust . Map.lookup 1) fls
-      firstElevator =
-        Elevator {efloor = 1, load = Elevatorload (head firstFloor)}
-   in State { elevator = firstElevator, floors = Map.insert 1 (tail firstFloor) fls, step = 0, history = []}
-
-solutionStep :: State -> [State]
-solutionStep State { elevator = e, floors = fls, step = s, history = his} =
-  let
-    his' = [unloadElevator e fls]
-    nextStep = -- maximumBy (\(e', x) (e'', x')-> compare (higherFloorIndex e' x) (higherFloorIndex e'' x')) $
-      nextSteps e fls his
-
-  in fmap (\(ens, fns) -> State { elevator = ens, floors = fns, step = s + 1, history = his ++ his' }) nextStep
-
-
-solution1 :: [State] -> Int
-solution1 sts --st@State { elevator = e, floors = fls, step = s }
-  | isJust end = (step . fromJust) end
-  | otherwise = solution1 $ concatMap solutionStep sts
-  where end = find (\st -> endCondition (elevator st) (floors st)) sts
-
--- solution1Index :: Int -> State  -> State
--- solution1Index index st@State { elevator = e, floors = fls, step = s }
---   | index == 0 = st
---   | otherwise = solution1Index (index - 1) (solutionStep st)
-
+-- Input ----------------------------------------------------------------------
 input :: IO Floors
 input = parseInput <$> readFile "input/2016/11December.txt"
 
@@ -167,7 +45,7 @@ parseRTG :: [String] -> [RTG]
 parseRTG [] = []
 parseRTG (x:y:xs) =
   if "microchip" `isPrefixOf` y
-    then Microchip ((fst . break ('-' ==)) x) : parseRTG xs
+    then Microchip (takeWhile ('-' /=) x) : parseRTG xs
     else Generator x : parseRTG xs
 
 inputTest :: Floors
@@ -178,12 +56,134 @@ inputTest =
 \The third floor contains a lithium generator.\n\
 \The fourth floor contains nothing relevant."
 
-solution1Test :: Bool
-solution1Test = (solution1 . (:[])  . stepZero) inputTest == 11
+-- ----- ------------------------------------------------------
+floorsEquality :: Floors -> Floors -> Bool
+floorsEquality fs1 fs2 = all (null . uncurry (\\)) $ elems fs1 `zip` elems fs2
+
+isMicrochip :: RTG -> Bool
+isMicrochip (Microchip _) = True
+isMicrochip _             = False
+
+validateRTG :: RTG -> RTG -> Bool
+validateRTG (Microchip _) (Microchip _)  = True
+validateRTG (Generator _) (Generator _)  = True
+validateRTG (Microchip s) (Generator s') = s == s'
+validateRTG (Generator s) (Microchip s') = s == s'
+
+validateFloor :: Int -> Floor -> Floors -> Bool
+validateFloor f l flrs = validateFloor' $ l ++ (fromJust . Map.lookup f) flrs
+
+validateFloor' :: [RTG] -> Bool
+validateFloor' fls = (all (`validate` fls) . filter isMicrochip) fls
+  where
+    validate :: RTG -> [RTG] -> Bool
+    validate r@(Microchip _) rs =
+      let generators = filter (not . isMicrochip) rs
+       in null generators || any (validateRTG r) generators
+    validate r@(Generator _) rs =
+      let chips = filter isMicrochip rs
+       in null chips || any (validateRTG r) chips
+
+buildElevator :: RTG -> RTG -> Maybe Floor
+buildElevator r r'
+  | validateRTG r r' = Just [r, r']
+  | otherwise = Nothing
+
+validLoads :: Floor -> [Floor]
+validLoads fls =
+  (mapMaybe
+     (\l ->
+        if length l == 1
+          then Just [head l]
+          else buildElevator (head l) (last l)) .
+   filter (\l -> ((`elem` [1, 2]) . length) l && validateFloor' (fls \\ l)) .
+   subsequences)
+    fls
+
+endCondition :: State -> Bool
+endCondition State {elevatorFloor = ef, floors = fs} =
+  ef == 4 &&
+  ((\l -> (null . concat . init) l && (not . null . last) l) . fmap snd . toList)
+    fs
+
+nextSteps :: State -> Set Floors -> [State]
+nextSteps State {elevatorFloor = f, floors = flrs} his =
+  let (previousFloor, currentFloor, nextFloor) =
+        (max (f - 1) 1, fromJust (Map.lookup f flrs), min (f + 1) 4)
+      nextValidLoads =
+        (sortOn (Data.Ord.Down . length) . validLoads) currentFloor
+      buildNextSteps =
+        \floorValue el ->
+          let newflv = adjust (\\ el) f flrs
+              newflv' = adjust (el ++) floorValue newflv
+           in if (not . (\x -> any (`floorsEquality` x) his)) newflv'
+                then Just State {elevatorFloor = floorValue, floors = newflv'}
+                else Nothing
+      nextLoadUp =
+        (mapMaybe (buildNextSteps nextFloor) .
+         filter (\el -> nextFloor /= f && validateFloor nextFloor el flrs))
+          nextValidLoads
+      nextLoadDown =
+        (mapMaybe (buildNextSteps previousFloor) .
+         filter
+           (\el ->
+              previousFloor /= f &&
+              (not .
+               null . concat . elems . takeWhileAntitone (previousFloor >=))
+                flrs &&
+              validateFloor previousFloor el flrs) .
+         reverse)
+          nextValidLoads
+   in if null nextLoadUp
+        then nextLoadDown
+        else nextLoadUp
+
+stepZero :: Floors -> State
+stepZero fls = State {elevatorFloor = 1, floors = fls}
+
+solution1 :: State -> Int -> MVar Int -> MVar (Set Floors) -> IO ()
+solution1 st step resultMV historyMV = do
+  history <- takeMVar historyMV
+  _ <- putMVar historyMV (Set.insert (floors st) history)
+  resultNotFound <- isEmptyMVar resultMV
+  --putStrLn $ show st ++ " - resultNotFound " ++ show resultNotFound
+  case (not resultNotFound, endCondition st) of
+    (_, True) -> do
+      previousResult <- tryTakeMVar resultMV
+      putStrLn $ "found a Result: " ++ show step
+      if isJust previousResult then putMVar resultMV (min step (fromJust previousResult)) else putMVar resultMV step
+    (True, _) -> do
+      result <- takeMVar resultMV
+      let isStepHigher = step >= result
+      --putStr $ show step ++ " "
+      _ <- putMVar resultMV result
+      if isStepHigher then return () else solution1Step st (step + 1) resultMV historyMV
+    _    -> solution1Step st (step + 1) resultMV historyMV
+
+solution1Step :: State -> Int -> MVar Int -> MVar (Set Floors) -> IO ()
+solution1Step st step resultMV historyMV = do
+  history <- takeMVar historyMV
+  let nextStates = nextSteps st history
+  _ <- putMVar historyMV history
+  mapM_ (\st' -> solution1 st' step resultMV historyMV) nextStates
+
+solution1Test :: IO Bool
+solution1Test = do
+  let firstState = stepZero inputTest
+  resultMV <- newEmptyMVar
+  historyMV <- newMVar empty
+  _ <- solution1 firstState 0 resultMV historyMV
+  value <- takeMVar resultMV
+  return $ value == 11
 
 eleventhDecemberSolution1 :: IO Int
-eleventhDecemberSolution1 =
-  solution1 . (:[]) . stepZero <$> input
+eleventhDecemberSolution1 = do
+  i <- input
+  let firstState = stepZero i
+  resultMV <- newEmptyMVar
+  historyMV <- newMVar empty
+  _ <- solution1 firstState 0 resultMV historyMV
+  takeMVar resultMV
 
 solution2 :: String -> Int
 solution2 = undefined
