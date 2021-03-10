843b4ddfc866ba3b38b5eda88b104bcf331d2c46
Enrico Benini <benkio89@gmail.com>
2021-03-10 20:26:29 +0000

    AoC 2016 day 22 p1 WIP

--- AdventOfCode/src/TwentySixteen/TwentysecondDecember.hs
+++ AdventOfCode/src/TwentySixteen/TwentysecondDecember.hs
@@ -1,15 +1,99 @@
 module TwentySixteen.TwentysecondDecember where
 
-input :: IO String
-input = readFile "input/2016/22December.txt"
+import Data.Map (Map, fromList, toList)
+import qualified Data.Map as Map
+import Text.Printf
 
-solution1 :: String -> Int
-solution1 = undefined
+type Coordinate = (Int, Int)
+
+type Grid = Map Coordinate Node
+
+data Node = Node
+  { size :: Int,
+    used :: Int,
+    avail :: Int,
+    usedPercent :: Int
+  }
+  deriving (Eq)
+
+instance Show Node where
+  show Node { size = s, used = u, avail = a, usedPercent = p } =
+    printf "Node: s %d - u %d - a %d - p %d%%" s u a p
+
+input :: IO Grid
+input = fromList . fmap parseNode . drop 2 . lines <$> readFile "input/2016/22December.txt"
+
+parseNode :: String -> (Coordinate, Node)
+parseNode =
+  ( \l ->
+      ( nodeNameToCoordinate (head l),
+        Node
+          { size = ((\x -> read x :: Int) . init) (l !! 1),
+            used = ((\x -> read x :: Int) . init) (l !! 2),
+            avail = ((\x -> read x :: Int) . init) (l !! 3),
+            usedPercent = ((\x -> read x :: Int) . init) (l !! 4)
+          }
+      )
+  )
+    . words
+
+nodeNameToCoordinate :: String -> (Int, Int)
+nodeNameToCoordinate s =
+  let x = ((\v -> read v :: Int) . takeWhile ('-' /=) . tail . dropWhile ('x' /=)) s
+      y = ((\v -> read v :: Int) . tail . dropWhile ('y' /=)) s
+   in (x, y)
+
+solution1 :: [Node] -> [(Node, Node)]
+solution1 ns = [(n1, n2) | n1 <- ns, n2 <- ns, n1 /= n2, used n1 /= 0, used n1 <= avail n2]
 
 twentysecondDecemberSolution1 :: IO Int
-twentysecondDecemberSolution1 = undefined
+twentysecondDecemberSolution1 = length . solution1 . fmap snd . toList <$> input
 
-solution2 :: String -> Int
+--
+-- ✓ 1 - Node list to Map by Coordinate
+-- ✓ 2 - Node to Node transfer
+-- ✓ 2b - possible transfers by neighboors
+-- 3 - Grid -> Node -> Set Grid generate the grids if I can move the node
+-- 4 - Solution: start from the target, generate the grids, if empty
+-- then generate the grids for the neighboors (recursive until you got
+-- something). Re apply solution with the target (if not moved) to the
+-- new grids. When target = (0,0) stop
+--
+-- History?? later maybe
+--
+
+target :: Coordinate
+target = (34, 0)
+
+nodeTransfer :: (Node, Node) -> (Node, Node)
+nodeTransfer (n1, n2) =
+  ( n1 {used = 0, avail = size n1, usedPercent = 0},
+    n2 {used = used n2 + used n1, avail = avail n2 - used n1, usedPercent = (used n2 + used n1) `div` (size n2 `div` 100)}
+  )
+
+possibleTransfers :: Map Coordinate Node -> [(Coordinate, Coordinate)]
+possibleTransfers grid =
+  let ns = toList grid
+  in [(c1, c2) | (c1, n1) <- ns, (c2, n2) <- ns, used n1 /= 0, used n1 <= avail n2, isNeighboor c1 c2]
+
+isNeighboor :: Coordinate -> Coordinate -> Bool
+isNeighboor (x, y) (x', y') = (x `elem` [x'-1, x'+1] && y == y') || (y `elem` [y'-1, y'+1] && x == x')
+
+inputTest :: Grid
+inputTest = (fromList . fmap parseNode . lines) "/dev/grid/node-x0-y0   10T    8T     2T   80%\n\
+\/dev/grid/node-x0-y1   11T    6T     5T   54%\n\
+\/dev/grid/node-x0-y2   32T   28T     4T   87%\n\
+\/dev/grid/node-x1-y0    9T    7T     2T   77%\n\
+\/dev/grid/node-x1-y1    8T    0T     8T    0%\n\
+\/dev/grid/node-x1-y2   11T    7T     4T   63%\n\
+\/dev/grid/node-x2-y0   10T    6T     4T   60%\n\
+\/dev/grid/node-x2-y1    9T    8T     1T   88%\n\
+\/dev/grid/node-x2-y2    9T    6T     3T   66%"
+
+test :: Bool
+test = solution2 inputTest == 7
+
+solution2 :: Grid -> Int
 solution2 = undefined
 
 twentysecondDecemberSolution2 :: IO Int
