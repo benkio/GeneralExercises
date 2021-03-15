04d895ddbd3b7d1f708651e4a22bc81bf498e700
Enrico Benini <benkio89@gmail.com>
2021-03-15 21:41:59 +0000

    AoC 2016 day 23 WIP 😠

--- AdventOfCode/src/TwentySixteen/TwentythirdDecember.hs
+++ AdventOfCode/src/TwentySixteen/TwentythirdDecember.hs
@@ -1,7 +1,43 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeApplications #-}
 module TwentySixteen.TwentythirdDecember where
 
-input :: IO String
-input = readFile "input/2016/23December.txt"
+import Control.Exception
+import Data.Maybe
+import Control.Monad
+import Data.List
+import qualified TwentySixteen.TwelfthDecember as T
+
+class ParsableInstruction a where
+  parse :: String -> a
+
+data SafeInstruction = forall t. (ParsableInstruction t, Show t) => SafeInstruction t
+data TGL = TGL String deriving Show
+
+instance ParsableInstruction T.Instruction where
+  parse = T.parseInstruction
+
+instance ParsableInstruction TGL where
+  parse s
+    | "tgl " `isPrefixOf` s = TGL $ drop 4 s
+    | otherwise = error "no TGL instruction"
+
+instance Show SafeInstruction where
+  show (SafeInstruction t) = show t
+
+input :: IO [SafeInstruction]
+input = do
+  i <- readFile "input/2016/23December.txt"
+  let is = lines i
+  mapM parseSafeInstruction is
+
+parseSafeInstruction :: String -> IO SafeInstruction
+parseSafeInstruction s = do
+  mayInstruction <- catch ((fmap (Just . SafeInstruction) . evaluate) (parse @T.Instruction s)) handler
+  mayTgl <- catch ((fmap (Just . SafeInstruction) . evaluate) (parse @TGL s)) handler
+  (return . fromJust) $ msum [mayInstruction, mayTgl]
+  where handler :: SomeException -> IO (Maybe SafeInstruction)
+        handler _ = return Nothing
 
 solution1 :: String -> Int
 solution1 = undefined
