b0a1f6e1f6ec06f16c3851aa686a646675487041
Enrico Benini <benkio89@gmail.com>
2021-03-20 16:46:37 +0000

    inspecting AoC day 24

--- AdventOfCode/src/TwentySixteen/TwentyfourthDecember.hs
+++ AdventOfCode/src/TwentySixteen/TwentyfourthDecember.hs
@@ -1,5 +1,20 @@
 module TwentySixteen.TwentyfourthDecember where
 
+-- the idea is to have a:
+--  - Map Coordinate Status indicate what is in the map
+--  - The initial position (Coordinate, Int) that is the position of the robot.
+--  - A function taking the list of Current Coordinates & the visited
+--    coordinates. It returns the available next coordinates that are not
+--    into the visited and the updated visited, adding the starting
+--    points to the visited.
+--  - Run the previous function with the initial position until a number
+--    is found. At each call increase a number that is the number of
+--    steps.
+--  - use the previous function again to find the first element to visit,
+--    collect it, update the position found with an empty space. Redo
+--    until all elements are reached.
+--  - sum up the steps to reach each elements. profit
+
 input :: IO String
 input = readFile "input/2016/24December.txt"
 
