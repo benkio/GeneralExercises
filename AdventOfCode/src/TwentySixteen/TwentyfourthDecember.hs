eb6de29c9fce68cbdac057bcdcc7d9ff32f8ec3b
Enrico Benini <benkio89@gmail.com>
2021-03-24 21:03:56 +0000

    memory expensive, but should work, I try with trees

--- AdventOfCode/src/TwentySixteen/TwentyfourthDecember.hs
+++ AdventOfCode/src/TwentySixteen/TwentyfourthDecember.hs
@@ -2,8 +2,9 @@
 
 module TwentySixteen.TwentyfourthDecember where
 
+import Data.List
 import Data.Map (Map, (!))
-import qualified Data.Map as Map (adjust, empty, filter, insert, toList)
+import qualified Data.Map as Map (empty, filter, insert, toList, union, fromList, keys)
 import Data.Set (Set, notMember)
 import qualified Data.Set as Set (empty, fromList, union)
 
@@ -60,29 +61,29 @@ maxCell = (176, 36)
 neighboors :: Coordinate -> [Coordinate]
 neighboors (x, y) = [(a, b) | a <- [max 0 (x -1) .. min (fst maxCell) (x + 1)], b <- [max 0 (y -1) .. min (snd maxCell) (y + 1)], (a == x || b == y) && (a /= x || b /= y)]
 
-robotSearch :: Grid -> Set Coordinate -> [Coordinate] -> Int -> IO (Coordinate, Int)
-robotSearch grid visited positions step
-  | (not . null) newHVAC = return (head newHVAC, step)
-  | otherwise = putStrLn ("visited: " ++ (show . length) visited) >> robotSearch grid (Set.union visited (Set.fromList positions)) nextPositions (step + 1)
+robotSearch :: Grid -> Set Coordinate -> [Coordinate] -> Coordinate -> Int -> IO Int
+robotSearch grid visited positions targetPosition step
+  | targetPosition `elem` positions = return step
+  | otherwise = putStrLn ("visited: " ++ (show . length) visited) >> robotSearch grid (Set.union visited (Set.fromList positions)) nextPositions targetPosition (step + 1)
   where
-    newHVAC = filter (\c -> isHVAC (grid ! c)) positions
     nextPositions = (filter (`notMember` visited) . concatMap neighboors) positions
 
-endCondition :: Grid -> Bool
-endCondition = null . Map.filter isHVAC
+buildGraph :: Grid -> [Coordinate] -> IO (Map (Coordinate, Coordinate) Int)
+buildGraph _ [] = return Map.empty
+buildGraph grid (h:hs) = do
+  partialGraph <- mapM (\(c,c') -> (\x -> [((c,c'),x), ((c',c),x)]) <$> robotSearch grid Set.empty [c] c' 0) $ replicate (length hs) h `zip` hs
+  Map.union ((Map.fromList . concat) partialGraph) <$> buildGraph grid hs
 
-buildGraph :: Grid -> Map (Coordinate, Coordinate) Int
-buildGraph hvacs = undefined
+computeSteps :: Map (Coordinate, Coordinate) Int -> [Coordinate] -> Int
+computeSteps graph hvacs = foldl (\acc x -> acc + (graph ! x)) 0 $ hvacs `zip` tail hvacs
 
--- search :: Grid -> Coordinate -> Int -> IO Int
--- search grid startingPoint acc
---   | endCondition grid' = return acc
---   | otherwise = do
---     (newPosition, steps) <- robotSearch grid' Set.empty [startingPoint] 0
---     putStrLn ("newPosition " ++ show newPosition ++ " steps " ++ show steps)
---     search grid' newPosition (acc + steps)
---   where
---     grid' = Map.adjust (const Open) startingPoint grid
+search :: Grid -> IO Int
+search grid = do
+  let hvacs = (Map.keys . Map.filter isHVAC) grid
+      initialRobotPosition = hvacPosition 0 grid
+  graph <- buildGraph grid hvacs
+  putStrLn $ show graph
+  ((\(p, x) -> print p >> return x ) . minimumBy (\(_, a) (_, b) -> compare a b) . fmap (\p -> (p, computeSteps graph p)) . filter (\p -> head p == initialRobotPosition) . permutations) hvacs
 
 inputTest :: Grid
 inputTest =
@@ -93,13 +94,11 @@ inputTest =
     \#4.......3#\n\
     \###########"
 
--- test :: IO Bool
--- test = (== 14) <$> search inputTest (0, 0) 0
+test :: IO Bool
+test = (== 14) <$> search inputTest
 
--- twentyfourthDecemberSolution1 :: IO Int
--- twentyfourthDecemberSolution1 = input >>= (\i -> search i (startingPoint i) 0)
---   where
---     startingPoint = hvacPosition 0
+twentyfourthDecemberSolution1 :: IO Int
+twentyfourthDecemberSolution1 = input >>= search
 
 solution2 :: String -> Int
 solution2 = undefined
