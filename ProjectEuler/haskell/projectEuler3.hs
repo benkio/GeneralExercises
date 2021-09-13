a20f8f679a3d9a2a4e17d1f1503a60cebeb5ee29
Enrico Benini <benkio89@gmail.com>
2021-09-13 16:10:36 +0100

    moved connect four + es 22 project euler

--- ProjectEuler/haskell/projectEuler3.hs
+++ ProjectEuler/haskell/projectEuler3.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ProjectEuler3 where
 
+import Data.List (find, sort)
+import Data.Maybe (fromJust, isJust)
+import qualified Data.Text as T (pack, replace, splitOn, unpack)
 import ProjectEuler2 (findDivisors)
-import Data.Maybe (isJust, fromJust)
 
 -- Es 21 ----------------------------------------------------------------------
 
@@ -9,15 +13,30 @@ amicableNum :: Int -> Maybe [Int]
 amicableNum x =
   let divisorSum = (sum . filter (/= x) . findDivisors) x
       amicableDivisorSum = (sum . filter (/= divisorSum) . findDivisors) divisorSum
-  in if amicableDivisorSum == x && x /= divisorSum then Just [x, divisorSum] else Nothing
+   in if amicableDivisorSum == x && x /= divisorSum then Just [x, divisorSum] else Nothing
 
 findAmicables :: [Int] -> [Int] -> [Int]
 findAmicables [] amicables = amicables
-findAmicables (x:xs) amicables
+findAmicables (x : xs) amicables
   | x `elem` amicables = findAmicables xs amicables
   | isJust maybeAmicables = findAmicables xs (amicables ++ fromJust maybeAmicables)
   | otherwise = findAmicables xs amicables
-    where maybeAmicables = amicableNum x
+  where
+    maybeAmicables = amicableNum x
 
 es21 :: Int
-es21 = sum $ findAmicables [1..10000] []
+es21 = sum $ findAmicables [1 .. 10000] []
+
+-- Es 22 ---------------------------------------------------------------
+
+es22Input :: IO [String]
+es22Input = sort . fmap (T.unpack . T.replace "\"" "") . T.splitOn "\",\"" . T.pack <$> readFile "p022_names.txt"
+
+alphabetIndexed :: [(Char, Int)]
+alphabetIndexed = ['A' .. 'Z'] `zip` [1 ..]
+
+nameScore :: String -> Int
+nameScore = sum . fmap (\c -> maybe 0 snd $ find ((== c) . fst) alphabetIndexed)
+
+es22 :: IO Int
+es22 = sum . zipWith (*) [1 ..] . fmap nameScore <$> es22Input
