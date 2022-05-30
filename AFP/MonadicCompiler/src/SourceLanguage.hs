18ed4f1c9c17c7987dc12d3df0d0041be89d941f
Enrico Benini <benkio89@gmail.com>
2022-05-30 20:00:54 +0100

    remove tabs to space

--- AFP/MonadicCompiler/src/SourceLanguage.hs
+++ AFP/MonadicCompiler/src/SourceLanguage.hs
@@ -1,13 +1,13 @@
 module SourceLanguage where
 
 data Prog = Assign Name Expr
-	  | If Expr Prog Prog
-	  | While Expr Prog
-	  | Seqn [Prog]
+          | If Expr Prog Prog
+          | While Expr Prog
+          | Seqn [Prog]
 
 data Expr = Val Int
-	  | Var Name
-	  | App Op Expr Expr
+          | Var Name
+          | App Op Expr Expr
 
 type Name = Char
 data Op = Add | Sub | Div | Mul
