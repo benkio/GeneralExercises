0f4bbf12b3dc7fd5728e363beb35a2c33bf6b119
Enrico Benini <benkio89@gmail.com>
2022-06-16 18:58:36 +0100

    Compilation refactoring done + started execution

--- AFP/MonadicCompiler/src/VirtualMachine.hs
+++ AFP/MonadicCompiler/src/VirtualMachine.hs
@@ -3,6 +3,8 @@ module VirtualMachine where
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State.Lazy
 import Control.Monad.Trans.Writer.Lazy
+import Data.Foldable (foldl')
+import Data.List (find)
 import SourceLanguage
 
 type Stack = [Int]
@@ -32,27 +34,20 @@ comp' :: Prog -> StateT Label (Writer Code) ()
 comp' (Assign name expr) = (lift . tell) $ compExpr expr ++ [POP name]
 comp' (Seqn []) = return ()
 comp' (Seqn (p : ps)) = comp' p >> comp' (Seqn ps)
-
--- comp' (If expr prog1 prog2) =
---   -- let (prog1Code, labelCount1) = comp' (labelCount + 2) prog1
---   --     (prog2Code, labelCount2) = comp' labelCount1 prog2
---   --  in ( compExpr expr
---   --         ++ [JUMPZ labelCount]
---   --         ++ prog1Code
---   --         ++ [JUMP (labelCount + 1), LABEL labelCount]
---   --         ++ prog2Code
---   --         ++ [LABEL (labelCount + 1)],
---   --       labelCount2
---   --     )
--- comp' labelCount (While expr prog) =
---   let (innerWhileCode, innerLabelCount) = comp' (labelCount + 2) prog
---    in ( [LABEL labelCount]
---           ++ compExpr expr
---           ++ [JUMPZ (labelCount + 1)]
---           ++ innerWhileCode
---           ++ [JUMP labelCount, LABEL (labelCount + 1)],
---         innerLabelCount
---       )
+comp' (While expr prog) = do
+  labelCount <- get
+  (lift . tell) $ [LABEL labelCount] ++ compExpr expr ++ [JUMPZ (labelCount + 1)]
+  put (labelCount + 2)
+  comp' prog
+  (lift . tell) [JUMP labelCount, LABEL (labelCount + 1)]
+comp' (If expr prog1 prog2) = do
+  labelCount <- get
+  (lift . tell) $ compExpr expr ++ [JUMPZ labelCount]
+  put (labelCount + 2)
+  comp' prog1
+  (lift . tell) $ [JUMP (labelCount + 1), LABEL labelCount]
+  comp' prog2
+  (lift . tell) [LABEL (labelCount + 1)]
 
 compExpr :: Expr -> Code
 compExpr (Val val) = [PUSH val]
@@ -68,4 +63,20 @@ emptyStack :: Stack
 emptyStack = []
 
 exec :: Code -> Mem
-exec = undefined
+exec code =
+  snd $ execState (foldl' (\s i -> s >> exec' i) (return ()) code) (emptyStack, emptyMem)
+
+exec' :: Inst -> State (Stack, Mem) ()
+exec' (PUSH value) = withState (\(st, mem) -> (value : st, mem)) (return ())
+exec' (PUSHV varName) =
+  withState
+    ( \(st, mem) ->
+        let newStack = (maybe st (\v -> v : st) . fmap snd . find (\(n, _) -> n == varName)) mem
+         in (newStack, mem)
+    )
+    (return ())
+exec' (POP varName) = undefined
+exec' (DO op) = undefined
+exec' (JUMP label) = undefined
+exec' (JUMPZ label) = undefined
+exec' (LABEL label) = undefined
