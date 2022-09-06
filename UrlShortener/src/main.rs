0a8500f81c7b7ae650a2b2757c7dd42c32b9c420
Enrico Benini <benkio89@gmail.com>
2022-09-06 20:00:00 +0100

    tests. `redirect`. `shorten` refactor. TODO: incrementally generate urls

--- UrlShortener/src/main.rs
+++ UrlShortener/src/main.rs
@@ -22,9 +22,9 @@ fn random_lowercase(max_length: usize) -> String {
     result
 }
 
-
 struct UrlShortener {
     url_database: HashMap<String, String>,
+    generated_links: u32,
 }
 
 
@@ -33,27 +33,98 @@ impl UrlShortener {
     fn new() -> Self {
         Self {
             url_database: HashMap::new(),
+            generated_links: 0,
         }
     }
 
 
-    fn shorten(&self, long_url: &str) -> String {
-        let max_length = 13 - URL_PREFIX.len();
-        if max_length <= 0 {
-            panic!("max_length is: {} and cannot be <= 0", max_length);
+    fn shorten(&mut self, long_url: &str) -> String {
+        match self.url_database.iter().find(|&(_, v)| v == long_url) {
+            Some((short_url, _)) => short_url.to_string(),
+            None => {
+            let max_length = 13 - URL_PREFIX.len();
+            if max_length <= 0 {
+                panic!("max_length is: {} and cannot be <= 0", max_length);
+            }
+            let prefix: String = URL_PREFIX.to_string().clone();
+            let postfix: String = random_lowercase(max_length);
+            let short_url: String = prefix + &postfix;
+            self.url_database.insert(short_url.clone(), long_url.to_string());
+            short_url
+            }
         }
-        let prefix: String = URL_PREFIX.to_string().clone();
-        let postfix: String = random_lowercase(max_length);
-        prefix + &postfix
     }
 
-    // fn redirect(&self, short_url: &str) -> String {
-    //     todo!()
-    // }
+    fn redirect(&self, short_url: &str) -> String {
+        self.url_database[short_url].to_string()
+    }
 }
 
 fn main() {
-    let url_shortener = UrlShortener::new();
+    let mut url_shortener = UrlShortener::new();
     let test = url_shortener.shorten("www.benkio.github.io/");
     println!("{}", test);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::UrlShortener;
+    use crate::assert_valid_short_url;
+
+    #[test]
+    fn two_different_urls() {
+        let mut url_shortener = UrlShortener::new();
+
+        let short_url_1 = url_shortener.shorten("https://www.codewars.com/kata/5ef9ca8b76be6d001d5e1c3e");
+        assert_valid_short_url!(&short_url_1);
+
+        let short_url_2 = url_shortener.shorten("https://www.codewars.com/kata/5efae11e2d12df00331f91a6");
+        assert_valid_short_url!(&short_url_2);
+
+        assert_eq!(url_shortener.redirect(&short_url_1), "https://www.codewars.com/kata/5ef9ca8b76be6d001d5e1c3e");
+        assert_eq!(url_shortener.redirect(&short_url_2), "https://www.codewars.com/kata/5efae11e2d12df00331f91a6");
+    }
+
+    #[test]
+    fn same_urls() {
+        let mut url_shortener = UrlShortener::new();
+
+        let short_url_1 = url_shortener.shorten("https://www.codewars.com/kata/5ef9c85dc41b4e000f9a645f");
+        assert_valid_short_url!(&short_url_1);
+
+        let short_url_2 = url_shortener.shorten("https://www.codewars.com/kata/5ef9c85dc41b4e000f9a645f");
+        assert_valid_short_url!(&short_url_2);
+
+        assert_eq!(short_url_1, short_url_2, "Should work with the same long URLs");
+        assert_eq!(
+            url_shortener.redirect(&short_url_1),
+            "https://www.codewars.com/kata/5ef9c85dc41b4e000f9a645f",
+            "{} should redirect to https://www.codewars.com/kata/5ef9c85dc41b4e000f9a645f",
+            &short_url_1,
+        );
+    }
+
+    #[macro_export]
+    macro_rules! assert_valid_short_url {
+        ($url:expr) => {
+            assert!(
+                $url.starts_with("short.ly/"),
+                "URL format is incorrect: should start with \"short.ly/\", got: {}",
+                $url,
+            );
+
+            assert!(
+                $url.len() < 14,
+                "URL format is incorrect: length should be < 14 characters, got: {}",
+                $url,
+            );
+
+            // As the URL contains "short.ly/", we can safely index using [9..]
+            assert!(
+                $url[9..].bytes().all(|b| b.is_ascii_lowercase()),
+                "URL format is incorrect: should contain lowercase letters at the end, got: {}",
+                $url,
+            );
+        }
+    }
 }
