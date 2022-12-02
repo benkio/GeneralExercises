4a7d17a434a0b08732226a24f40f8b51948342db
Enrico Benini <benkio89@gmail.com>
2022-12-02 19:01:05 +0000

    AoC 2022 1 & 2

--- AdventOfCode/src/TwentyTwentyTwo/FirstDecember.hs
+++ AdventOfCode/src/TwentyTwentyTwo/FirstDecember.hs
@@ -1,13 +1,34 @@
 module TwentyTwentyTwo.FirstDecember where
 
-input :: IO [Int]
-input = fmap (\x -> (read x :: Int)) . lines <$> readFile "input/2022/1December.txt"
+import Data.Maybe (maybe)
+import Text.Read (readMaybe)
+import Data.List
 
-testInput :: [Int]
-testInput = undefined
+input :: IO [String]
+input = lines <$> readFile "input/2022/1December.txt"
+
+testInput :: String
+testInput =
+  "1000\n\
+  \2000\n\
+  \3000\n\
+  \\n\
+  \4000\n\
+  \\n\
+  \5000\n\
+  \6000\n\
+  \\n\
+  \7000\n\
+  \8000\n\
+  \9000\n\
+  \\n\
+  \10000"
+
+inputParse :: [String] -> [[Int]]
+inputParse = (\(acc, last) -> acc ++ [last]) . foldl (\(acc, lacc) sv -> (maybe (acc ++ [lacc], []) (\v -> (acc, lacc ++ [v])) . readMaybe) sv) ([], [])
 
 firstDecemberSolution1 :: IO Int
-firstDecemberSolution1 = undefined
+firstDecemberSolution1 = (maximum . fmap sum . inputParse) <$> input
 
 firstDecemberSolution2 :: IO Int
-firstDecemberSolution2 = undefined
+firstDecemberSolution2 = (sum . take 3 . sortBy (flip compare) . fmap sum . inputParse) <$> input
