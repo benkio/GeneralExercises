ce2ff67ba5e6157dbceabb9c774ebc61798596ed
Enrico Benini <benkio89@gmail.com>
2022-12-04 19:34:03 +0000

    AoC 2022 Day 4

--- AdventOfCode/src/TwentyTwentyTwo/FourthDecember.hs
+++ AdventOfCode/src/TwentyTwentyTwo/FourthDecember.hs
@@ -1,13 +1,49 @@
 module TwentyTwentyTwo.FourthDecember where
 
-input :: IO [Int]
-input = fmap (\x -> (read x :: Int)) . lines <$> readFile "input/2022/4December.txt"
+import Data.Bifunctor (bimap)
+import Data.List (isSubsequenceOf)
 
-testInput :: [Int]
-testInput = undefined
+data Assignment = Assignment
+  { first :: [Int],
+    second :: [Int]
+  }
+  deriving (Show)
+
+input :: IO [Assignment]
+input = parseInput . lines <$> readFile "input/2022/4December.txt"
+
+parseInput :: [String] -> [Assignment]
+parseInput = fmap parseAssignemnt
+  where
+    parseAssignemnt :: String -> Assignment
+    parseAssignemnt = (\(r1, r2) -> Assignment {first = r1, second = r2}) . bimap stringToRange (stringToRange . tail) . break (== ',')
+    stringToRange :: String -> [Int]
+    stringToRange = (\(lb, hb) -> enumFromTo lb hb) . bimap (\x -> read x :: Int) (\x -> read (tail x) :: Int) . break (== '-')
+
+testInput :: [Assignment]
+testInput =
+  (parseInput . lines)
+    "2-4,6-8\n\
+    \2-3,4-5\n\
+    \5-7,7-9\n\
+    \2-8,3-7\n\
+    \6-6,4-6\n\
+    \2-6,4-8"
+
+assignmentContained :: Assignment -> Bool
+assignmentContained Assignment {first = f, second = s} = isSubsequenceOf f s || isSubsequenceOf s f
+
+solution1 :: [Assignment] -> Int
+solution1 = length . filter id . fmap assignmentContained
 
 fourthDecemberSolution1 :: IO Int
-fourthDecemberSolution1 = undefined
+fourthDecemberSolution1 = solution1 <$> input
+
+assignmentOverlap :: Assignment -> Bool
+assignmentOverlap Assignment {first = f, second = s} = any (`elem` s) f
+
+solution2 :: [Assignment] -> Int
+solution2 = length . filter id . fmap assignmentOverlap
 
 fourthDecemberSolution2 :: IO Int
-fourthDecemberSolution2 = undefined
+fourthDecemberSolution2 = solution2 <$> input
