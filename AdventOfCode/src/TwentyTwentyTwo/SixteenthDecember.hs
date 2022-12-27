9e755193b99cebf6f6c7a2513c87092f155594d7
Enrico Benini <benkio89@gmail.com>
2022-12-27 22:52:02 +0000

    another attempt, good on testinput, not on input

--- AdventOfCode/src/TwentyTwentyTwo/SixteenthDecember.hs
+++ AdventOfCode/src/TwentyTwentyTwo/SixteenthDecember.hs
@@ -1,55 +1,109 @@
 module TwentyTwentyTwo.SixteenthDecember where
 
-import Text.Printf (printf)
-
-import Data.Bifunctor (second)
-import Data.List (groupBy, maximumBy, minimumBy, partition, sort, sortOn, (\\))
-import Data.Map (Map, delete, elems, empty, fromList, insert, keys, lookup, size, (!))
+import Data.Bifunctor (bimap)
+import Data.List (groupBy, minimumBy, partition, sortOn, (\\))
+import Data.Map (Map, elems, empty, filterWithKey, fromList, toList, unionWith, (!))
+import Data.Tree (Tree, foldTree, unfoldTree)
 import Debug.Trace
-import Prelude hiding (lookup)
+import Text.Printf (printf)
 
 data Valve = Valve
     { name :: String
     , rate :: Int
     , connections :: [String]
     }
-    deriving (Show, Eq, Ord)
+    deriving (Eq, Ord)
 
 data State = State
-    { currentValve :: String
-    , valves :: [String]
-    , timeSpent :: Int
+    { currentValve :: Valve
+    , openValves :: [Valve]
     , total :: Int
+    , cost :: Int
     }
-    deriving (Show)
 
-initialState :: String -> State
-initialState sv = State{currentValve = sv, valves = [], timeSpent = 0, total = 0}
-
-addStepState :: ValveMap -> (String, String) -> (Int, Int) -> State -> State
-addStepState vm (sv, ev) (c, r) s =
-    State
-        { currentValve = ev
-        , valves = sv : (valves s)
-        , timeSpent = c + timeSpent s
-        , total = (releasedPressure vm (valves s)) * c + total s + r
-        }
+instance Show Valve where
+    show v = printf "V: %s %d %s" (name v) (rate v) ((show . connections) v)
+instance Show State where
+    show s = printf "S: %d %d %s %s" (total s) (cost s) ((name . currentValve) s) ((show . fmap name . openValves) s)
 
 type ValveMap = Map String Valve
-type BestPaths = Map String State
 type ConnectionCostMap = Map (String, String) (Int, Int)
 
 input :: IO ValveMap
 input = parseInput <$> readFile "input/2022/16December.txt"
 
-connectionMap :: ValveMap -> ConnectionCostMap
-connectionMap vm = (fromList . innerSearch . filter (\v -> ((> 0) . rate) v || name v == "AA") . elems) vm
+flowRatesValves :: ValveMap -> [Valve]
+flowRatesValves = filter ((> 0) . rate) . elems
+
+startingState :: ValveMap -> State
+startingState = (\v -> State{currentValve = v, total = 0, cost = 0, openValves = []}) . (! "AA")
+
+move :: ValveMap -> ConnectionCostMap -> State -> [State]
+move vm ccn (State{currentValve = cv, openValves = ovs, total = t, cost = c})
+    | length ovs == length (flowRatesValves vm) || c >= 30 = []
+    | otherwise = connectionStates
   where
-    innerSearch :: [Valve] -> [((String, String), (Int, Int))]
-    innerSearch [] = []
+    newTotal c = t + ((sum . fmap rate) ovs) * c
+    connectionStates =
+        ( fmap (\((_, e), (c, _)) -> buildState e c)
+            . toList
+            . (\n -> filterWithKey (\(s, e) _ -> s == n && e `notElem` (fmap name ovs)) ccn)
+            . name
+        )
+            cv
+    buildState end cost =
+        State
+            { currentValve = vm ! end
+            , cost = c + cost
+            , total = newTotal cost
+            , openValves = (vm ! end) : ovs
+            }
+
+pathTree :: ValveMap -> ConnectionCostMap -> State -> Tree State
+pathTree vm ccm s = unfoldTree (\x -> (x, move vm ccm x)) s
+
+extendToTime :: Int -> State -> State
+extendToTime time (State{cost = ts, total = t, openValves = vs, currentValve = cv}) =
+    State
+        { currentValve = cv
+        , cost = time
+        , openValves = vs
+        , total = t + ((time - ts) * ((sum . fmap rate) vs))
+        }
+
+solution1 :: ValveMap -> Int
+solution1 vm = foldTree comparePaths tree
+  where
+    tree = pathTree vm (connectionMap vm) (startingState vm)
+
+comparePaths :: State -> [Int] -> Int
+comparePaths s [] = (total . extendToTime 30) s
+comparePaths _ childs = maximum childs
+
+-- 1947
+sixteenthDecemberSolution1 :: IO Int
+sixteenthDecemberSolution1 = solution1 <$> input
+
+sixteenthDecemberSolution2 :: IO Int
+sixteenthDecemberSolution2 = undefined
+
+-- connectionMap ---------------------------------
+
+connectionMap :: ValveMap -> ConnectionCostMap
+connectionMap vm = (innerSearch . filter (\v -> ((> 0) . rate) v || name v == "AA") . elems) vm
+  where
+    innerSearch :: [Valve] -> ConnectionCostMap
+    innerSearch [] = empty
     innerSearch (v : vs) =
-        let vrs = (\(v', c) -> ((name v, name v'), (c, c * rate v))) <$> reachableValves ([], 0) v vm
-         in vrs ++ innerSearch vs
+        let vrs = fromList $ (\(v', c) -> ((name v, name v'), (c, c * rate v))) <$> reachableValves ([], 0) v vm
+         in unionWith
+                ( \(c, v) (c', v') -> case c `compare` c' of
+                    GT -> (c, v)
+                    EQ -> (c, v)
+                    LT -> (c', v')
+                )
+                vrs
+                (innerSearch vs)
 
 reachableValves :: ([String], Int) -> Valve -> ValveMap -> [(Valve, Int)]
 reachableValves prev v vm = (fmap ((\(_, c, v) -> (v, c)) . minimumBy (\(_, c, _) (_, c', _) -> c `compare` c')) . groupBy (\(_, _, v) (_, _, v') -> v == v') . sortOn (\(_, _, v) -> v)) $ reachableValves' prev v vm
@@ -61,68 +115,7 @@ reachableValves' (prevValves, cost) v@Valve{connections = cv, name = vn} vm =
     (nextValvesEmpty, nextValvesNonEmpty) = (partition (\v -> (rate v == 0 || name v == "AA")) . fmap (vm !) . (\\ prevValves)) cv
     keepLooking = concatMap (\x -> reachableValves' (prevValves ++ [vn], cost + 1) x vm) (nextValvesEmpty ++ nextValvesNonEmpty)
 
-log' f x = trace (f x) x
-
-search :: ValveMap -> ConnectionCostMap -> State
-search vm cm =
-    let startingState = initialState "AA"
-     in ( maximumBy (\s s' -> total s `compare` total s')
-            . fmap (extendToTime vm 30)
-            . traceShowId
-            . elems
-        )
-            $ searchR [startingState] empty cm vm
-
-searchR :: [State] -> BestPaths -> ConnectionCostMap -> ValveMap -> BestPaths
-searchR sts bsp cm vm =
-    let next = sts >>= \s -> nextStep s vm cm bsp
-        nextBest = (fmap (bestState vm) . groupBy (\s s' -> currentValve s == currentValve s') . sortOn currentValve) next
-        bsb' = foldl (\bs s -> insert (currentValve s) s bs) bsp nextBest
-     in if null (log' (show . length) next)
-            then bsp
-            else searchR next bsb' cm vm
-
-nextStep :: State -> ValveMap -> ConnectionCostMap -> BestPaths -> [State]
-nextStep st vm cm bsp =
-    ( filter (\s -> (timeSpent s <= 30))
-        . fmap (\(x, y) -> addStepState vm (x, y) (cm ! (x, y)) st)
-        . filter (\(s, e) -> s == (currentValve st) && e `notElem` (valves st))
-        . keys
-    )
-        cm
-
-compareState :: ValveMap -> State -> State -> Ordering
-compareState vm s s' =
-    let maxTime = max (timeSpent s) (timeSpent s')
-     in total (extendToTime vm maxTime s) `compare` total (extendToTime vm maxTime s')
-
-bestState :: ValveMap -> [State] -> State
-bestState _ (s : []) = s
-bestState vm (x : y : xs) =
-    let s = if compareState vm x y == GT then x else y
-     in bestState vm (s : xs)
-
-releasedPressure :: ValveMap -> [String] -> Int
-releasedPressure vm av = sum $ fmap (rate . (vm !)) av
-
-extendToTime :: ValveMap -> Int -> State -> State
-extendToTime vm time (State{timeSpent = ts, total = t, valves = vs, currentValve = cv}) =
-    State
-        { currentValve = cv
-        , timeSpent = time
-        , valves = vs
-        , total = t + ((time - ts) * releasedPressure vm (cv : vs))
-        }
-
-solution1 :: ValveMap -> Int
-solution1 vm = (total . traceShowId . search vm . connectionMap) vm
-
--- 1909 too low
-sixteenthDecemberSolution1 :: IO Int
-sixteenthDecemberSolution1 = solution1 <$> input
-
-sixteenthDecemberSolution2 :: IO Int
-sixteenthDecemberSolution2 = undefined
+-- Parse -----------------------------------------
 
 parseInput :: String -> ValveMap
 parseInput = fromList . fmap parseValve . lines
