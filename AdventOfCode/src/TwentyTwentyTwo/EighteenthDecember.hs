dc1113527bd52866745e742d619b3afbf5319854
Enrico Benini <benkio89@gmail.com>
2023-01-26 11:42:53 +0000

    AoC day 18 🌟

--- AdventOfCode/src/TwentyTwentyTwo/EighteenthDecember.hs
+++ AdventOfCode/src/TwentyTwentyTwo/EighteenthDecember.hs
@@ -1,13 +1,60 @@
 module TwentyTwentyTwo.EighteenthDecember where
 
-input :: IO [Int]
-input = fmap (\x -> (read x :: Int)) . lines <$> readFile "input/2022/18December.txt"
+import Data.List (group, sort)
 
-testInput :: [Int]
-testInput = undefined
+type Obsidian = (Int, Int, Int)
+type ObsidianFace = (Float, Float, Float)
+
+parseInput = fmap parseObsidian . lines
+  where
+    parseX = takeWhile (/= ',')
+    parseY = parseX . tail . dropWhile (/= ',')
+    parseZ = parseY . tail . dropWhile (/= ',')
+    parseObsidian s =
+        ( read (parseX s) :: Int
+        , read (parseY s) :: Int
+        , read (parseZ s) :: Int
+        )
+
+input :: IO [Obsidian]
+input = parseInput <$> readFile "input/2022/18December.txt"
+
+testInput :: [Obsidian]
+testInput =
+    parseInput
+        "2,2,2\n\
+        \1,2,2\n\
+        \3,2,2\n\
+        \2,1,2\n\
+        \2,3,2\n\
+        \2,2,1\n\
+        \2,2,3\n\
+        \2,2,4\n\
+        \2,2,6\n\
+        \1,2,5\n\
+        \3,2,5\n\
+        \2,1,5\n\
+        \2,3,5"
+
+computeFaces :: Obsidian -> [ObsidianFace]
+computeFaces (a, b, c) =
+    [ (x + 0.5, y, z)
+    , (x - 0.5, y, z)
+    , (x, y + 0.5, z)
+    , (x, y - 0.5, z)
+    , (x, y, z + 0.5)
+    , (x, y, z - 0.5)
+    ]
+  where
+    x = fromIntegral a :: Float
+    y = fromIntegral b :: Float
+    z = fromIntegral c :: Float
+
+solution1 :: [Obsidian] -> Int
+solution1 = sum . fmap length . filter ((== 1) . length) . group . sort . concatMap computeFaces
 
 eighteenthDecemberSolution1 :: IO Int
-eighteenthDecemberSolution1 = undefined
+eighteenthDecemberSolution1 = solution1 <$> input
 
 eighteenthDecemberSolution2 :: IO Int
 eighteenthDecemberSolution2 = undefined
