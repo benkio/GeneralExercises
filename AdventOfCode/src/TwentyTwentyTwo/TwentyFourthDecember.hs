501bb27985152d12522e3be36c09cf2105a53196
Enrico Benini <benkio89@gmail.com>
2023-03-21 19:19:23 +0000

    AoC 24 WIP

--- AdventOfCode/src/TwentyTwentyTwo/TwentyFourthDecember.hs
+++ AdventOfCode/src/TwentyTwentyTwo/TwentyFourthDecember.hs
@@ -1,13 +1,13 @@
 module TwentyTwentyTwo.TwentyFourthDecember where
 
-import Data.Map (Map, empty, fromList, union)
+import Data.Map (Map, empty, fromList, union, foldrWithKey)
 
-data Field = NB | SB | EB | WB deriving (Show)
+data Blizzard = NB | SB | EB | WB deriving (Show)
 data Move = NM | SM | EM | WM | W deriving (Show)
 type Position = (Int, Int)
 data Valley = Valley
     { entrance :: Position
-    , valley :: Map Position Field
+    , valleyBlizzards :: Map Position Blizzard
     , valleyBounds :: Position
     , exit :: Position
     , expedition :: Position
@@ -17,15 +17,25 @@ data Valley = Valley
 input :: IO Valley
 input = parseInput <$> readFile "input/2022/24December.txt"
 
-testInput :: Valley
-testInput =
-    parseInput
-        "#.######\n\
-        \#>>.<^<#\n\
-        \#.<..<<#\n\
-        \#>v.><>#\n\
-        \#<^v^^>#\n\
-        \######.#"
+valleyMove :: Valley -> Valley
+valleyMove v@(Valley {valleyBlizzards = vbm, valleyBounds = vbs}) =
+  v { valleyBlizzards = vbm'}
+  where
+    vbm' = undefined
+
+blizzardMove :: Position -> Position -> Blizzard -> Position
+blizzardMove vbounds (x, y) NB = wrap vbounds (x, y - 1)
+blizzardMove vbounds (x, y) SB = wrap vbounds (x, y + 1)
+blizzardMove vbounds (x, y) EB = wrap vbounds (x + 1, y)
+blizzardMove vbounds (x, y) WB = wrap vbounds (x - 1, y)
+
+wrap :: Position -> Position -> Position
+wrap (vbx, vby) (x, y)
+    | x < 0 = (vbx, y)
+    | x > vbx = (0, y)
+    | y < 0 = (x, vby)
+    | y > vby = (x, 0)
+    | otherwise = (x, y)
 
 twentyFourthDecemberSolution1 :: IO Int
 twentyFourthDecemberSolution1 = undefined
@@ -43,9 +53,15 @@ parseInput s =
             ( ((\x -> x - 3) . length . head) ls
             , ((\x -> x - 1) . length . init . tail) ls
             )
-     in Valley{entrance = (s_entrance, (-1)), expedition = (s_entrance, (-1)), valley = s_valley, exit = (s_exit, length ls - 2), valleyBounds = s_valleyBounds}
+     in Valley
+            { entrance = (s_entrance, (-1))
+            , expedition = (s_entrance, (-1))
+            , valleyBlizzards = s_valley
+            , exit = (s_exit, length ls - 2)
+            , valleyBounds = s_valleyBounds
+            }
 
-parseValley :: [String] -> Map Position Field
+parseValley :: [String] -> Map Position Blizzard
 parseValley =
     fst
         . foldl
@@ -56,7 +72,7 @@ parseValley =
             )
             (empty, 0)
 
-parseValleyRow :: String -> Int -> [(Position, Field)]
+parseValleyRow :: String -> Int -> [(Position, Blizzard)]
 parseValleyRow s y = go s 0
   where
     go [] x = []
@@ -73,3 +89,13 @@ parseTopBottom s = go s 0
     go [] i = error "Entrance/exit not found"
     go ('#' : xs) i = go xs (i + 1)
     go ('.' : _) i = i - 1
+
+testInput :: Valley
+testInput =
+    parseInput
+        "#.######\n\
+        \#>>.<^<#\n\
+        \#.<..<<#\n\
+        \#>v.><>#\n\
+        \#<^v^^>#\n\
+        \######.#"
