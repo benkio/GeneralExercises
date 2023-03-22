c9a7ccac689a49f76160a2f46c82dc4cade3fb93
Enrico Benini <benkio89@gmail.com>
2023-03-22 20:40:13 +0000

    AoC 24 WIP

--- AdventOfCode/src/TwentyTwentyTwo/TwentyFourthDecember.hs
+++ AdventOfCode/src/TwentyTwentyTwo/TwentyFourthDecember.hs
@@ -1,33 +1,60 @@
 module TwentyTwentyTwo.TwentyFourthDecember where
 
-import Data.Map (Map, empty, fromList, union, foldrWithKey)
-
-data Blizzard = NB | SB | EB | WB deriving (Show)
+data Blizzard = NB Position | SB Position | EB Position | WB Position deriving (Show, Eq)
 data Move = NM | SM | EM | WM | W deriving (Show)
 type Position = (Int, Int)
 data Valley = Valley
     { entrance :: Position
-    , valleyBlizzards :: Map Position Blizzard
+    , valleyBlizzards :: [Blizzard]
     , valleyBounds :: Position
     , exit :: Position
-    , expedition :: Position
     }
     deriving (Show)
+
+-- This indetifies a configuration fully, the minute is the index of the valleys in the env
+-- Eq intance. State with the same position and minute % valleys should be the same!!!
+data State = State {currPos :: Position, minute :: Int}
+
+-- Used when generating the tree of states in search of the goal
+data Env = Env {valleys :: [[Blizzard]], target :: Position, visited :: [State], vBounds :: Position}
 
 input :: IO Valley
 input = parseInput <$> readFile "input/2022/24December.txt"
 
-valleyMove :: Valley -> Valley
-valleyMove v@(Valley {valleyBlizzards = vbm, valleyBounds = vbs}) =
-  v { valleyBlizzards = vbm'}
-  where
-    vbm' = undefined
+-- Generate the next states by generating the next possible positions given the current blizzard
+-- and filters by the visited states
+nextStates :: State -> Env -> [State]
+nextStates (State{currPos = crp, minute = m}) (Env{valleys = vss, target = ext, visited = vis, vBounds = vbs}) = undefined
 
-blizzardMove :: Position -> Position -> Blizzard -> Position
-blizzardMove vbounds (x, y) NB = wrap vbounds (x, y - 1)
-blizzardMove vbounds (x, y) SB = wrap vbounds (x, y + 1)
-blizzardMove vbounds (x, y) EB = wrap vbounds (x + 1, y)
-blizzardMove vbounds (x, y) WB = wrap vbounds (x - 1, y)
+-- Generate new states based on where expedition can go, or just wait where it is
+expeditionMove :: State -> [Blizzard] -> [State]
+expeditionMove (State{currPos = crp, minute = m}) bs = undefined
+
+eqState :: Int -> State -> State -> Bool
+eqState valleyNum (State{currPos = cr1, minute = m1}) (State{currPos = cr2, minute = m2}) =
+    cr1 == cr2 && m1 `mod` valleyNum == m2 `mod` valleyNum
+
+valleyToSearchInit :: Valley -> (State, Env)
+valleyToSearchInit v@(Valley{entrance = ent, valleyBounds = vbs, exit = ext}) =
+    ( State{currPos = ent, minute = 0}
+    , Env{valleys = allValleys v, target = ext, visited = [], vBounds = vbs}
+    )
+
+-- blizzard configurations are cyclic, so we can think about generating all the configs once and for all.
+allValleys :: Valley -> [[Blizzard]]
+allValleys v =
+    let vss = valleyBlizzards <$> iterate valleyMove v
+     in valleyBlizzards v : takeWhile (/= valleyBlizzards v) (tail vss)
+
+valleyMove :: Valley -> Valley
+valleyMove v@(Valley{valleyBlizzards = vbm, valleyBounds = vbs}) =
+    v{valleyBlizzards = fmap (blizzardMove vbs) vbm}
+
+blizzardMove :: Position -> Blizzard -> Blizzard
+blizzardMove vbounds (NB (x, y)) = NB $ wrap vbounds (x, y - 1)
+blizzardMove vbounds (SB (x, y)) = SB $ wrap vbounds (x, y + 1)
+blizzardMove vbounds (EB (x, y)) = EB $ wrap vbounds (x - 1, y)
+blizzardMove vbounds (WB (x, y)) = WB $ wrap vbounds (x + 1, y)
 
 wrap :: Position -> Position -> Position
 wrap (vbx, vby) (x, y)
@@ -36,6 +63,8 @@ wrap (vbx, vby) (x, y)
     | y < 0 = (x, vby)
     | y > vby = (x, 0)
     | otherwise = (x, y)
+
+test = (!! 5) $ iterate valleyMove testSmall -- (!! 1000) . iterate valleyMove <$> input
 
 twentyFourthDecemberSolution1 :: IO Int
 twentyFourthDecemberSolution1 = undefined
@@ -55,33 +84,32 @@ parseInput s =
             )
      in Valley
             { entrance = (s_entrance, (-1))
-            , expedition = (s_entrance, (-1))
             , valleyBlizzards = s_valley
             , exit = (s_exit, length ls - 2)
             , valleyBounds = s_valleyBounds
             }
 
-parseValley :: [String] -> Map Position Blizzard
+parseValley :: [String] -> [Blizzard]
 parseValley =
     fst
         . foldl
             ( \(m, y) l ->
-                ( union m $ fromList $ parseValleyRow l y
+                ( m ++ parseValleyRow l y
                 , y + 1
                 )
             )
-            (empty, 0)
+            ([], 0)
 
-parseValleyRow :: String -> Int -> [(Position, Blizzard)]
+parseValleyRow :: String -> Int -> [Blizzard]
 parseValleyRow s y = go s 0
   where
     go [] x = []
     go ('#' : xs) x = go xs x
     go ('.' : xs) x = go xs (x + 1)
-    go ('>' : xs) x = ((x, y), WB) : go xs (x + 1)
-    go ('<' : xs) x = ((x, y), EB) : go xs (x + 1)
-    go ('v' : xs) x = ((x, y), SB) : go xs (x + 1)
-    go ('^' : xs) x = ((x, y), NB) : go xs (x + 1)
+    go ('>' : xs) x = WB (x, y) : go xs (x + 1)
+    go ('<' : xs) x = EB (x, y) : go xs (x + 1)
+    go ('v' : xs) x = SB (x, y) : go xs (x + 1)
+    go ('^' : xs) x = NB (x, y) : go xs (x + 1)
 
 parseTopBottom :: String -> Int
 parseTopBottom s = go s 0
@@ -99,3 +127,20 @@ testInput =
         \#>v.><>#\n\
         \#<^v^^>#\n\
         \######.#"
+
+testSmall :: Valley
+testSmall =
+    parseInput
+        "#.#####\n\
+        \#.....#\n\
+        \#>....#\n\
+        \#.....#\n\
+        \#...v.#\n\
+        \#.....#\n\
+        \#####.#\n"
+
+blizzardPos :: Blizzard -> Position
+blizzardPos (NB p) = p
+blizzardPos (SB p) = p
+blizzardPos (EB p) = p
+blizzardPos (WB p) = p
