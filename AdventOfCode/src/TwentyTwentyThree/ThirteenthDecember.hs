d91fbeb9fd9edfa84c8490c9849cf2ffeec49694
Enrico Benini <benkio89@gmail.com>
2024-01-14 13:42:53 +0100

    Day 13 second star

--- AdventOfCode/src/TwentyTwentyThree/ThirteenthDecember.hs
+++ AdventOfCode/src/TwentyTwentyThree/ThirteenthDecember.hs
@@ -1,8 +1,10 @@
 module TwentyTwentyThree.ThirteenthDecember where
 
+import Control.Applicative ((<|>))
 import Data.Bifunctor (bimap)
-import Data.List (find, groupBy, transpose)
-import Data.Maybe (fromMaybe, listToMaybe, mapMaybe)
+import Data.List (delete, find, groupBy, transpose)
+import Data.Maybe (catMaybes, fromMaybe, listToMaybe, mapMaybe)
+import Debug.Trace
 
 type Field = [String]
 
@@ -54,16 +56,30 @@ calculateV f = do
     (l, _) <- findHReflection $ transpose f
     return (length l)
 
-solution1 :: [Field] -> Int
-solution1 =
+solution :: (Field -> Field) -> [Field] -> Int
+solution f =
     sum
-        . fmap (\f -> fromMaybe 0 (calculateH f) + fromMaybe 0 (calculateV f))
+        . traceShowId
+        . fmap (calculateReflection . f)
+  where
+    calculateReflection f = fromMaybe 0 $ (calculateH f) <|> (calculateV f)
 
--- 22288 too low
 thirdteenthDecemberSolution1 :: IO Int
-thirdteenthDecemberSolution1 = solution1 <$> input
+thirdteenthDecemberSolution1 = solution id <$> input
 
-solution2 = undefined
+solution2 :: [Field] -> Int
+solution2 = sum . mapMaybe (\f -> findSmudgeIndex f calcH <|> findSmudgeIndex (transpose f) calcV)
+  where calcH i = 100 * i
+        calcV = id
 
+findSmudgeIndex :: Field -> (Int -> Int) -> Maybe Int
+findSmudgeIndex field f = (fmap f . find checkIndex) [1..length field - 1]
+  where
+    topReflection i = take i field
+    bottomReflection i = drop i field
+    checkIndex i = ((==1) . sum) $ (\(l,l') -> (length . filter (==False) . fmap (uncurry (==))) (zip l l'))  <$> zip (reverse (topReflection i)) (bottomReflection i)
+
+-- right answer: 25401
+-- [9, 10, 7, 100, 1400, 1, 500, 6, 7, 6, 1300, 300, 900, 1, 100, 200, 800, 1, 500, 2, 800, 8, 9, 700, 9, 6, 1200, 1500, 12, 5, 100, 6, 10, 9, 200, 1300, 100, 500, 600, 12, 9, 100, 100, 12, 500, 9, 400, 600, 1, 1, 1200, 200, 8, 100, 7, 500, 800, 12, 800, 500, 200, 12, 4, 5, 500, 4, 800, 16, 5, 6, 500, 1, 13, 200, 400, 800, 3, 3, 700, 12, 9, 800, 8, 9, 9, 3, 200, 500, 5, 500, 16, 3, 12, 3, 8, 7, 7, 14, 8, 1]
 thirdteenthDecemberSolution2 :: IO Int
-thirdteenthDecemberSolution2 = undefined
+thirdteenthDecemberSolution2 = solution2 <$> input
