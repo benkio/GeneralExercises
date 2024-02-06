014bf40a31f11ba1bb91aa12280c9ac18858a95c
Enrico Benini <benkio89@gmail.com>
2024-02-06 00:36:37 +0000

    WIP Day 16

--- AdventOfCode/src/TwentyTwentyThree/SixteenthDecember.hs
+++ AdventOfCode/src/TwentyTwentyThree/SixteenthDecember.hs
@@ -1,12 +1,86 @@
+{-# LANGUAGE TupleSections #-}
+
 module TwentyTwentyThree.SixteenthDecember where
 
-input :: IO String
+import Data.Map (Map, fromList)
+import qualified Data.Map as M (lookup)
+import Data.Set (Set)
+
+type Cave = Map (Int, Int) Tile
+data Direction = U | D | R | L
+data Tile
+    = E
+    | VS
+    | HS
+    | MR
+    | ML
+    deriving (Eq)
+
+instance Show Tile where
+    show E = "."
+    show VS = "|"
+    show HS = "-"
+    show MR = "/"
+    show ML = "\\"
+
+input :: IO Cave
 input = parseInput <$> readFile "input/2023/16December.txt"
 
-parseInput = undefined
+parseInput :: String -> Cave
+parseInput =
+    fromList
+        . concatMap
+            ( \(r, s) ->
+                ( fmap (\(c, c') -> ((c, r), parseTile c'))
+                    . zip [0 ..]
+                )
+                    s
+            )
+        . zip [0 ..]
+        . lines
 
-testInput :: String
-testInput = parseInput ""
+parseTile :: Char -> Tile
+parseTile '.' = E
+parseTile '|' = VS
+parseTile '-' = HS
+parseTile '/' = MR
+parseTile '\\' = ML
+
+testInput :: Cave
+testInput =
+    parseInput $ unlines [".|...\\....", "|.-.\\.....", ".....|-...", "........|.", "..........", ".........\\", "..../.\\\\..", ".-.-/..|..", ".|....-|.\\", "..//.|...."]
+
+newDirections :: Direction -> Tile -> [Direction]
+newDirections R VS = [U, D]
+newDirections L VS = [U, D]
+newDirections U HS = [R, L]
+newDirections D HS = [R, L]
+newDirections R MR = [U]
+newDirections L MR = [D]
+newDirections U MR = [R]
+newDirections D MR = [L]
+newDirections R ML = [D]
+newDirections L ML = [U]
+newDirections U ML = [L]
+newDirections D ML = [R]
+newDirections d _ = [d] -- E or pointy end of a splitter
+
+newCoord :: (Int, Int) -> Direction -> (Int, Int)
+newCoord (x, y) U = (x, y + 1)
+newCoord (x, y) D = (x, y - 1)
+newCoord (x, y) L = (x - 1, y)
+newCoord (x, y) R = (x + 1, y)
+
+newTiles :: Cave -> (Int, Int) -> Direction -> [((Int, Int), Direction)]
+newTiles cs c d = (nextCord,) <$> nextDirs
+  where
+    nextCord = newCoord c d
+    nextTile = M.lookup nextCord cs
+    nextDirs = maybe [] (newDirections d) nextTile
+
+-- lightBeamBounce :: Set (Int, Int) -> Cave -> (Int, Int) -> Direction -> (Set (Int, Int), (Int,Int), Direction)
+-- lightBeamBounce vs cs c d = undefined
+--   where nts = filter (`notMember` vs) $ newTiles cs c d
 
 solution1 = undefined
 
