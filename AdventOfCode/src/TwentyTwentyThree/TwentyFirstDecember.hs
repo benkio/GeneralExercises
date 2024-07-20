ae059b9d78871e6527b14bccd3e8a83eb0d65f51
Enrico Benini <benkio89@gmail.com>
2024-07-20 14:50:46 +0100

    reformat

--- AdventOfCode/src/TwentyTwentyThree/TwentyFirstDecember.hs
+++ AdventOfCode/src/TwentyTwentyThree/TwentyFirstDecember.hs
@@ -1,42 +1,42 @@
 module TwentyTwentyThree.TwentyFirstDecember where
 
-import qualified Data.Map as M (fromList, lookup, toList, filter)
 import Data.Map (Map)
+import qualified Data.Map as M (filter, fromList, lookup, toList)
 import Data.Set (Set)
-import qualified Data.Set as S (map, fromList, toList, size)
+import qualified Data.Set as S (fromList, map, size, toList)
 
 data FieldBlock = GardenPlot | Rock | StartingPosition deriving (Show, Eq)
 data Step = N | S | E | W deriving (Show, Enum)
 type Field = Map (Int, Int) FieldBlock
 
-setConcatMap :: Ord b => (a->[b]) -> Set a -> Set b
+setConcatMap :: (Ord b) => (a -> [b]) -> Set a -> Set b
 setConcatMap f = S.fromList . concatMap f . S.toList
 
 input :: IO Field
 input = parseInput <$> readFile "input/2023/21December.txt"
 
-isGardenPlot :: Field -> (Int,Int) -> Bool
-isGardenPlot f x = maybe False (==GardenPlot) $ M.lookup x f
-isStartingPosition :: Field -> (Int,Int) -> Bool
-isStartingPosition f x = maybe False (==StartingPosition) $ M.lookup x f
+isGardenPlot :: Field -> (Int, Int) -> Bool
+isGardenPlot f x = maybe False (== GardenPlot) $ M.lookup x f
+isStartingPosition :: Field -> (Int, Int) -> Bool
+isStartingPosition f x = maybe False (== StartingPosition) $ M.lookup x f
 
 takeStep :: (Int, Int) -> Step -> (Int, Int)
-takeStep (x,y) N = (x, y - 1)
-takeStep (x,y) S = (x, y + 1)
-takeStep (x,y) E = (x - 1, y)
-takeStep (x,y) W = (x + 1, y)
+takeStep (x, y) N = (x, y - 1)
+takeStep (x, y) S = (x, y + 1)
+takeStep (x, y) E = (x - 1, y)
+takeStep (x, y) W = (x + 1, y)
 
-takeOneStep :: Field -> Set (Int,Int) -> Set (Int, Int)
+takeOneStep :: Field -> Set (Int, Int) -> Set (Int, Int)
 takeOneStep f s = setConcatMap (\c -> filter (\x -> isGardenPlot f x || isStartingPosition f x) (takeStep c <$> (enumFrom N))) s
 
-walk :: Field -> Set (Int,Int) -> Int -> Set (Int, Int)
+walk :: Field -> Set (Int, Int) -> Int -> Set (Int, Int)
 walk _ s 0 = s
-walk f s c = walk f (takeOneStep f s) (c-1)
+walk f s c = walk f (takeOneStep f s) (c - 1)
 
 solution1 f = S.size $ walk f startingPoint 64
   where
     startingPoint :: Set (Int, Int)
-    startingPoint = S.fromList . map fst . M.toList $ M.filter (==StartingPosition) f
+    startingPoint = S.fromList . map fst . M.toList $ M.filter (== StartingPosition) f
 
 twentyfirstDecemberSolution1 :: IO Int
 twentyfirstDecemberSolution1 = solution1 <$> input
@@ -47,11 +47,12 @@ twentyfirstDecemberSolution2 :: IO Int
 twentyfirstDecemberSolution2 = undefined
 
 parseInput :: String -> Field
-parseInput = M.fromList . concat . zipWith (\y ls -> zipWith (\x c -> ((x,y), fromChar c)) [0..] ls) [0..] . lines
-  where fromChar :: Char -> FieldBlock
-        fromChar '.' = GardenPlot
-        fromChar '#' = Rock
-        fromChar 'S' = StartingPosition
+parseInput = M.fromList . concat . zipWith (\y ls -> zipWith (\x c -> ((x, y), fromChar c)) [0 ..] ls) [0 ..] . lines
+  where
+    fromChar :: Char -> FieldBlock
+    fromChar '.' = GardenPlot
+    fromChar '#' = Rock
+    fromChar 'S' = StartingPosition
 
 testInput :: Field
 testInput =
