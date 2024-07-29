eb1e99d613f201f2f5e2cd6822772cee3c8cd89f
Enrico Benini <benkio89@gmail.com>
2024-07-29 12:21:11 +0100

    Day 23 ⭐ WIP

--- AdventOfCode/src/TwentyTwentyThree/TwentyThirdDecember.hs
+++ AdventOfCode/src/TwentyTwentyThree/TwentyThirdDecember.hs
@@ -1,12 +1,71 @@
+{-# LANGUAGE LambdaCase #-}
+
 module TwentyTwentyThree.TwentyThirdDecember where
 
-input :: IO String
+import Data.Map (Map, findMax)
+import qualified Data.Map as M (fromList)
+import Data.Maybe (catMaybes)
+
+data FieldBlock = Path | SlopeU | SlopeL | SlopeD | SlopeR
+type HikeMap = Map (Int, Int) FieldBlock
+
+instance Show FieldBlock where
+    show Path = "."
+    show SlopeU = "^"
+    show SlopeR = ">"
+    show SlopeD = "v"
+    show SlopeL = "<"
+
+instance Read FieldBlock where
+    readsPrec _ = \case
+        '.' : xs -> [(Path, xs)]
+        '^' : xs -> [(SlopeU, xs)]
+        '>' : xs -> [(SlopeR, xs)]
+        'v' : xs -> [(SlopeD, xs)]
+        '<' : xs -> [(SlopeL, xs)]
+        xs -> []
+
+input :: IO HikeMap
 input = parseInput <$> readFile "input/2023/23December.txt"
 
-parseInput = undefined
+parseInput :: String -> HikeMap
+parseInput = M.fromList . concat . zipWith parseLine [0 ..] . lines
+  where
+    parseLine :: Int -> String -> [((Int, Int), FieldBlock)]
+    parseLine y = catMaybes . zipWith (\x c -> if c /= '#' then Just ((x, y), read [c]) else Nothing) [0 ..]
 
-testInput :: String
-testInput = parseInput ""
+startingPoint :: (Int, Int)
+startingPoint = (1, 0)
+
+endingPoint :: HikeMap -> (Int,Int)
+endingPoint = fst . findMax
+
+testInput :: HikeMap
+testInput =
+    parseInput
+        "#.#####################\n\
+        \#.......#########...###\n\
+        \#######.#########.#.###\n\
+        \###.....#.>.>.###.#.###\n\
+        \###v#####.#v#.###.#.###\n\
+        \###.>...#.#.#.....#...#\n\
+        \###v###.#.#.#########.#\n\
+        \###...#.#.#.......#...#\n\
+        \#####.#.#.#######.#.###\n\
+        \#.....#.#.#.......#...#\n\
+        \#.#####.#.#.#########v#\n\
+        \#.#...#...#...###...>.#\n\
+        \#.#.#v#######v###.###v#\n\
+        \#...#.>.#...>.>.#.###.#\n\
+        \#####v#.#.###v#.#.###.#\n\
+        \#.....#...#...#.#.#...#\n\
+        \#.#########.###.#.#.###\n\
+        \#...###...#...#...#.###\n\
+        \###.###.#.###v#####v###\n\
+        \#...#...#.#.>.>.#.>.###\n\
+        \#.###.###.#.###.#.#v###\n\
+        \#.....###...###...#...#\n\
+        \#####################.#"
 
 solution1 = undefined
 
