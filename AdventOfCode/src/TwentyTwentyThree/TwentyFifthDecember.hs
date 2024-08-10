1d8ac41ca5f28c47ff698eb91e197880d770338c
Enrico Benini <benkio89@gmail.com>
2024-08-11 00:12:58 +0300

    Day 25 parse input

--- AdventOfCode/src/TwentyTwentyThree/TwentyFifthDecember.hs
+++ AdventOfCode/src/TwentyTwentyThree/TwentyFifthDecember.hs
@@ -1,12 +1,38 @@
 module TwentyTwentyThree.TwentyFifthDecember where
 
-input :: IO String
+import Data.Map (Map, insertWith, empty)
+
+import Data.List.Split (splitOn)
+
+import Data.Bifunctor (bimap, first, second)
+
+input :: IO (Map String [String])
 input = parseInput <$> readFile "input/2023/25December.txt"
 
-parseInput = undefined
+parseInput :: String -> Map String [String]
+parseInput = foldl (\m (x, v) -> insertWith (++) x [v] m) empty .
+  concatMap parseLineWire . lines
+  where
+    parseLineWire :: String -> [(String, String)]
+    parseLineWire =
+        (\(s, xs) -> concatMap (\x -> [(s, x), (x, s)]) xs) . second (splitOn " " . drop 2) . break (== ':')
 
-testInput :: String
-testInput = parseInput ""
+testInput :: Map String [String]
+testInput =
+    parseInput
+        "jqt: rhn xhk nvd\n\
+        \rsh: frs pzl lsr\n\
+        \xhk: hfx\n\
+        \cmg: qnr nvd lhk bvb\n\
+        \rhn: xhk bvb hfx\n\
+        \bvb: xhk hfx\n\
+        \pzl: lsr hfx nvd\n\
+        \qnr: nvd\n\
+        \ntq: jqt hfx bvb xhk\n\
+        \nvd: lhk\n\
+        \lsr: lhk\n\
+        \rzs: qnr cmg lsr rsh\n\
+        \frs: qnr lhk lsr"
 
 solution1 = undefined
 
