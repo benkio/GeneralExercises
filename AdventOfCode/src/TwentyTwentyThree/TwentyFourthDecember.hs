338840b04512f83579cc22ad527d6bee9cf1dc73
Enrico Benini <benkio89@gmail.com>
2024-08-10 22:32:59 +0300

    Day 24 parsing input

--- AdventOfCode/src/TwentyTwentyThree/TwentyFourthDecember.hs
+++ AdventOfCode/src/TwentyTwentyThree/TwentyFourthDecember.hs
@@ -1,12 +1,52 @@
 module TwentyTwentyThree.TwentyFourthDecember where
 
-input :: IO String
+import Data.List.Split (splitOn)
+
+import Data.Bifunctor (bimap)
+
+data PointVelocity = PV
+    { px :: Int
+    , py :: Int
+    , pz :: Int
+    , vx :: Int
+    , vy :: Int
+    , vz :: Int
+    }
+    deriving (Show)
+
+input :: IO [PointVelocity]
 input = parseInput <$> readFile "input/2023/24December.txt"
 
-parseInput = undefined
+parseInput :: String -> [PointVelocity]
+parseInput =
+    fmap
+        ( listsToPointVelocity
+            . bimap
+                (fmap (\x -> read x :: Int) . splitOn ", ")
+                (fmap (\x -> read x :: Int) . splitOn ", " . drop 2)
+            . break (== '@')
+        )
+        . lines
+  where
+    listsToPointVelocity :: ([Int], [Int]) -> PointVelocity
+    listsToPointVelocity ([x, y, z], [vx', vy', vz']) =
+        PV
+            { px = x
+            , py = y
+            , pz = z
+            , vx = vx'
+            , vy = vy'
+            , vz = vz'
+            }
 
-testInput :: String
-testInput = parseInput ""
+testInput :: [PointVelocity]
+testInput =
+    parseInput
+        "19, 13, 30 @ -2,  1, -2\n\
+        \18, 19, 22 @ -1, -1, -2\n\
+        \20, 25, 34 @ -2, -2, -4\n\
+        \12, 31, 28 @ -1, -2, -1\n\
+        \20, 19, 15 @  1, -5, -3"
 
 solution1 = undefined
 
