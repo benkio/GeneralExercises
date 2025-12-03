1864ac87d4419cea128098c12e1fcc5dddd354a2
Enrico Benini <benkio89@gmail.com>
2025-12-03 23:17:20 +0000

    Remove AdventOfCode - Moved to a private repo

--- AdventOfCode/src/Lib/Move.hs
+++
@@ -1,9 +0,0 @@
-module Lib.Move (Move (..)) where
-
-data Move = U | D | L | R deriving (Eq, Ord)
-
-instance Show Move where
-    show U = "^"
-    show D = "v"
-    show L = "<"
-    show R = ">"
--- AdventOfCode/src/Lib/String.hs
+++
@@ -1,7 +0,0 @@
-module Lib.String (bytestringToString) where
-
-import qualified Data.ByteString as DBS
-import Text.Printf (printf)
-
-bytestringToString :: DBS.ByteString -> String
-bytestringToString = concatMap (printf "%02x") . DBS.unpack
